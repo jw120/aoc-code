a6f651441f77b19c026d07f5844d165ccb1df553
Joe Watson <joe_watson@mail.com>
2020-12-07 09:23:27 +0800

    Refactored InCode to remove Instruction class

--- python/IntCode.py
+++ python/IntCode.py
@@ -4,138 +4,7 @@ from __future__ import annotations
 
 from doctest import testmod
 from enum import Enum
-from typing import ClassVar, Dict, List, Optional, Tuple
-
-
-class Instruction:
-    """Represents an element of the IntCode Machine's instruction set.
-
-    Used via its subclasses which represent specific instructions. Not
-    intended to be instantiated (just used as classes)
-
-    Each instruction takes two arguments and returns one (which may be
-    None) unless it is a special case.
-
-    is_input    If true, calling machine should use its external input
-                as the return value from the instruction, rather than
-                calling execute()
-
-    is_output   If true, calling machine should pass the argument to
-                its external output, rather than calling execute()
-
-    is_halt     If true, calling machine should halt rather than calling
-                excute()
-
-    In addition one flag determines how the calling machine should interpret
-    the return value from execute()
-
-    is_jump     If true, the calling machine should interpret a non-None
-                return value as the new instruction pointer
-
-    Finally, disassembly_name and dissamebly_params are used when disassembling the
-    instruction. disassembly_params is the number of parameters shown
-    """
-
-    opcode: ClassVar[int]
-
-    disassembly_name: ClassVar[str]
-    disassembly_params: ClassVar[int]
-
-    is_input: ClassVar[bool] = False
-    is_output: ClassVar[bool] = False
-    is_halt: ClassVar[bool] = False
-    is_jump: ClassVar[bool] = False
-
-    @classmethod
-    def execute(self, arg1: int, arg2: int) -> Optional[int]:
-        raise RuntimeError("Called base instruction execute")
-
-
-class Add_Ins(Instruction):
-    opcode: ClassVar[int] = 1
-    disassembly_name: ClassVar[str] = "add"
-    disassembly_params: ClassVar[int] = 3
-
-    @classmethod
-    def execute(self, x: int, y: int) -> Optional[int]:
-        return x + y
-
-
-class Mul_Ins(Instruction):
-    opcode: ClassVar[int] = 2
-    disassembly_name: ClassVar[str] = "mul"
-    disassembly_params: ClassVar[int] = 3
-
-    @classmethod
-    def execute(self, x: int, y: int) -> Optional[int]:
-        return x * y
-
-
-class Inp_Ins(Instruction):
-    opcode: ClassVar[int] = 3
-    disassembly_name: ClassVar[str] = "inp"
-    disassembly_params: ClassVar[int] = 1
-    is_input: ClassVar[bool] = True
-
-
-class Out_Ins(Instruction):
-    opcode: ClassVar[int] = 4
-    disassembly_name: ClassVar[str] = "out"
-    disassembly_params: ClassVar[int] = 1
-    is_output: ClassVar[bool] = True
-
-
-class Jmp_Ins(Instruction):
-    opcode: ClassVar[int] = 5
-    disassembly_name: ClassVar[str] = "jmp"
-    disassembly_params: ClassVar[int] = 3
-    is_jump: ClassVar[bool] = True
-
-    @classmethod
-    def execute(self, cond: int, target: int) -> Optional[int]:
-        if cond != 0:
-            return target
-        return None
-
-
-class Jmz_Ins(Instruction):
-    opcode: ClassVar[int] = 6
-    disassembly_name: ClassVar[str] = "jmz"
-    disassembly_params: ClassVar[int] = 3
-    is_jump: ClassVar[bool] = True
-
-    @classmethod
-    def execute(self, cond: int, target: int) -> Optional[int]:
-        if cond == 0:
-            return target
-        return None
-
-
-class Les_Ins(Instruction):
-    opcode: ClassVar[int] = 7
-    disassembly_name: ClassVar[str] = "les"
-    disassembly_params: ClassVar[int] = 3
-
-    @classmethod
-    def execute(self, x: int, y: int) -> Optional[int]:
-        return x < y
-
-
-class Equ_Ins(Instruction):
-    opcode: ClassVar[int] = 8
-    disassembly_name: ClassVar[str] = "equ"
-    disassembly_params: ClassVar[int] = 3
-
-    @classmethod
-    def execute(self, x: int, y: int) -> Optional[int]:
-        return x == y
-
-
-class Halt_Ins(Instruction):
-    opcode: ClassVar[int] = 99
-    disassembly_name: ClassVar[str] = "halt"
-    disassembly_params: ClassVar[int] = 0
-    is_halt: ClassVar[bool] = True
+from typing import Callable, ClassVar, Dict, List, Optional, Tuple
 
 
 class Mode(Enum):
@@ -175,21 +44,44 @@ class Mode(Enum):
 class Machine:
     """Machine that runs IntCode."""
 
+    # Special opcode names (used in matching)
+    Input_opcode: ClassVar[int] = 3
+    Output_opcode: ClassVar[int] = 4
+    Halt_opcode: ClassVar[int] = 99
+
+    # Opcode names (used for disassembly)
+    Opcode_names: ClassVar[Dict[int, str]] = {
+        1: "add",
+        2: "mul",
+        Input_opcode: "input",
+        Output_opcode: "output",
+        5: "jmp",
+        6: "jmz",
+        7: "les",
+        8: "equ",
+        Halt_opcode: "halt",
+    }
+
+    # Jump opcodes
+    Jump_instructions: ClassVar[Dict[int, Callable[[int, int], Optional[int]]]] = {
+        5: lambda x, y: y if x != 0 else None,
+        6: lambda x, y: y if x == 0 else None,
+    }
+
+    # Arithmetic opcodes
+    Arithmetic_instructions: ClassVar[Dict[int, Callable[[int, int], int]]] = {
+        1: lambda x, y: x + y,
+        2: lambda x, y: x * y,
+        7: lambda x, y: x < y,
+        8: lambda x, y: x == y,
+    }
+
     def __init__(self, code: List[int], input_val: Optional[int] = None) -> None:
         self.code: List[int] = code.copy()
         self.ip: int = 0
         self.input_val: Optional[int] = input_val
         self.output_vals: List[int] = []
         self.halted: bool = False
-        self.instruction_set: Dict[int, Instruction] = {
-            ins.opcode: ins() for ins in Instruction.__subclasses__()
-        }
-
-    def _decode(self, opcode: int) -> Instruction:
-        """Lookup an opcode ."""
-        if opcode in self.instruction_set:
-            return self.instruction_set[opcode]
-        raise RuntimeError("Unknown opcode", opcode)
 
     def _fetch(self, mode: Mode, x: int) -> int:
         """Retrieve a value with the given mode."""
@@ -202,21 +94,28 @@ class Machine:
     def _print(self, comment: str = "") -> None:
         """Print the current instruction and comment."""
         instruction_code: int = self.code[self.ip]
-        instruction: Instruction = self._decode(instruction_code % 100)
-        (mode1, mode2) = Mode.modes(instruction_code)
-        modes: str = (
-            "_" + Mode.tag(mode1) + Mode.tag(mode2)
-            if instruction.disassembly_params > 0
-            else ""
-        )
-        name: str = instruction.disassembly_name + modes
-        args = ", ".join(
-            str(x)
-            for x in self.code[
-                self.ip + 1 : self.ip + 1 + instruction.disassembly_params
-            ]
-        )
-        print(f"{self.ip:4} {name:6} {args:20} {comment}")
+        opcode: int = instruction_code % 100
+        if opcode not in self.Opcode_names:
+            print("Unknown opcode in _print", opcode)
+            return
+        instruction_name: str = self.Opcode_names[opcode]
+        if opcode == self.Halt_opcode:
+            instruction_params: int = 0
+            modes: str = ""
+            args: str = ""
+        elif opcode == self.Input_opcode or opcode == self.Output_opcode:
+            instruction_params = 1
+            modes = ""
+            args = str(self.code[self.ip + 1])
+        else:
+            instruction_params = 2
+            (mode1, mode2) = Mode.modes(instruction_code)
+            modes = "_" + Mode.tag(mode1) + Mode.tag(mode2)
+            args = ", ".join(
+                str(x)
+                for x in self.code[self.ip + 1 : self.ip + 1 + instruction_params]
+            )
+        print(f"{self.ip:4} {instruction_name + modes:6} {args:20} {comment}")
 
     def step(self, quiet: bool = True) -> None:
         """Run one instruction."""
@@ -228,9 +127,9 @@ class Machine:
 
     def _step(self, quiet: bool = True) -> None:
         instruction_code: int = self.code[self.ip]
-        instruction: Instruction = self._decode(instruction_code % 100)
+        opcode: int = instruction_code % 100
 
-        if instruction.is_input:
+        if opcode == self.Input_opcode:
             if self.input_val is None:
                 raise RuntimeError("No input available")
             self.code[self.code[self.ip + 1]] = self.input_val
@@ -240,7 +139,7 @@ class Machine:
             self.ip += 2
             return
 
-        if instruction.is_output:
+        if opcode == self.Output_opcode:
             mode = Mode.mode(instruction_code)
             arg = self._fetch(mode, self.code[self.ip + 1])
             self.output_vals.append(arg)
@@ -249,31 +148,34 @@ class Machine:
             self.ip += 2
             return
 
-        if instruction.is_halt:
+        if opcode == self.Halt_opcode:
             self.halted = True
             if not quiet:
                 self._print("halted")
             return
 
-        # Now we have either a jump or a normal instruction (add/mul/etc)
         (mode1, mode2) = Mode.modes(instruction_code)
-        arg1 = self._fetch(mode1, self.code[self.ip + 1])
-        arg2 = self._fetch(mode2, self.code[self.ip + 2])
-        return_value: Optional[int] = instruction.execute(arg1, arg2)
+        arg1: int = self._fetch(mode1, self.code[self.ip + 1])
+        arg2: int = self._fetch(mode2, self.code[self.ip + 2])
 
-        if instruction.is_jump:
+        if opcode in self.Jump_instructions:
+            jump_value: Optional[int] = self.Jump_instructions[opcode](arg1, arg2)
             if not quiet:
-                self._print()
-            self.ip = self.ip + 3 if return_value is None else return_value
-        else:
-            if return_value is None:
-                raise RuntimeError("None returned")
+                self._print(f"Jump to {jump_value}" if jump_value else "Jump skipped")
+            self.ip = self.ip + 3 if jump_value is None else jump_value
+            return
+
+        if opcode in self.Arithmetic_instructions:
+            return_value: int = self.Arithmetic_instructions[opcode](arg1, arg2)
             if not quiet:
                 self._print(
-                    f"{arg1} {arg2} = {return_value} -> {self.code[self.ip + 3]}"
+                    f"{self.Opcode_names[opcode]} {arg1} {arg2} = {return_value} -> {self.code[self.ip + 3]}"
                 )
             self.code[self.code[self.ip + 3]] = return_value
             self.ip += 4
+            return
+
+        raise RuntimeError("Unknown opcode in step", opcode)
 
     def run(self, quiet: bool = True) -> None:
         """Run until execution stops."""
