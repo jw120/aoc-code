66ea31450a92638ab6ed9d6a7f98120e68e39721
Joe Watson <joe_watson@mail.com>
2020-12-08 20:49:14 +0800

    Done 2019 day 8

--- python/aoc_2019_08.py
+++ python/aoc_2019_08.py
@@ -22,17 +22,25 @@ def part_one(i: Image) -> int:
     layer_counts: List[Dict[str, int]] = []
     for layer in range(0, i.layers):
         layer_counts.append({})
-        layer_counts[layer]["0"] = 0
-        layer_counts[layer]["1"] = 0
-        layer_counts[layer]["2"] = 0
         for row in range(0, i.rows):
             for col in range(0, i.cols):
                 p: str = i.pixel(layer, row, col)
-                layer_counts[layer][p] += 1
+                layer_counts[layer][p] = layer_counts[layer].get(p, 0) + 1
     min_layer = min(layer_counts, key=lambda x: x["0"])
     return min_layer["1"] * min_layer["2"]
+
+
+def part_two(i: Image) -> None:
+    for row in range(0, i.rows):
+        for col in range(0, i.cols):
+            layer = 0
+            while i.pixel(layer, row, col) == "2":
+                layer += 1
+            print("*" if (i.pixel(layer, row, col) == "1") else " ", end="")
+        print()
 
 
 if __name__ == "__main__":
     i = Image(stdin.read().strip(), 6, 25)
     print(part_one(i))
+    part_two(i)
