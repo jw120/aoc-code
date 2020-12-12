b330d8e487b1a5055130ae8700dfba62c9ce3528
Joe Watson <joe_watson@mail.com>
2020-12-12 20:38:53 +0800

    Tidied 2020 day 12

--- python/aoc_2020_12.py
+++ python/aoc_2020_12.py
@@ -3,96 +3,63 @@
 from __future__ import annotations
 
 from doctest import testmod
-from enum import Enum, auto
 from sys import stdin
 from typing import Iterable, List, Tuple
 
 
-class Command(Enum):
-    """Commands given to the ship."""
+def rotate(s: str, rotation: int) -> str:
+    """Rotate a direction the number of degrees to the right.
 
-    NORTH = auto()
-    EAST = auto()
-    SOUTH = auto()
-    WEST = auto()
-    LEFT = auto()
-    RIGHT = auto()
-    FORWARD = auto()
-
-    def left(self) -> Command:
-        if self == Command.NORTH:
-            return Command.WEST
-        if self == Command.EAST:
-            return Command.NORTH
-        if self == Command.SOUTH:
-            return Command.EAST
-        if self == Command.WEST:
-            return Command.SOUTH
-        raise RuntimeError("Bad command to rotate", self)
+    >>> rotate("N", 90)
+    'E'
+    """
+    i = "NESW".find(s)
+    if i == -1:
+        raise RuntimeError("Bad direction")
+    return "NESW"[(i + (rotation // 90)) % 4]
 
 
-def parse_command(s: str) -> Tuple[Command, int]:
-    param = int(s[1:])
-    if s[0] == "N":
-        return (Command.NORTH, param)
-    if s[0] == "E":
-        return (Command.EAST, param)
-    if s[0] == "S":
-        return (Command.SOUTH, param)
-    if s[0] == "W":
-        return (Command.WEST, param)
-    if s[0] == "F":
-        return (Command.FORWARD, param)
-    if s[0] == "L":
-        return (Command.LEFT, param)
-    if s[0] == "R":
-        return (Command.RIGHT, param)
-    raise RuntimeError("Bad command to parse", s)
+def parse_command(s: str) -> Tuple[str, int]:
+    return (s[0], int(s[1:]))
 
 
 test1 = [parse_command(s) for s in ["F10", "N3", "F7", "R90", "F11"]]
 
 
 class Ship:
-    """Basic ship for part one"""
+    """Basic ship for part one."""
 
     def __init__(self) -> None:
         self.x = 0
         self.y = 0
-        self.facing = Command.EAST
+        self.facing = "E"
 
-    def action(self, com: Command, param: int) -> Ship:
+    def action(self, com: str, param: int) -> Ship:
         """Update the ship based on the action.
 
-        >>> Ship().action(Command.FORWARD, 10).x
+        >>> Ship().action("F", 10).x
         10
         """
-        if com == Command.NORTH:
+        if com == "N":
             self.y += param
-        elif com == Command.EAST:
+        elif com == "E":
             self.x += param
-        elif com == Command.SOUTH:
+        elif com == "S":
             self.y -= param
-        elif com == Command.WEST:
+        elif com == "W":
             self.x -= param
-        elif com == Command.FORWARD:
+        elif com == "F":
             self.action(self.facing, param)
-        elif (com == Command.LEFT and param == 90) or (
-            com == Command.RIGHT and param == 270
-        ):
-            self.facing = self.facing.left()
-        elif (com == Command.LEFT or com == Command.RIGHT) and param == 180:
-            self.facing = self.facing.left().left()
-        elif (com == Command.RIGHT and param == 90) or (
-            com == Command.LEFT and param == 270
-        ):
-            self.facing = self.facing.left().left().left()
+        elif com == "L":
+            self.facing = rotate(self.facing, -param)
+        elif com == "R":
+            self.facing = rotate(self.facing, param)
         else:
             raise RuntimeError("Bad command for action", com, param)
         #        print(com, param, self.x, self.y, self.facing)
         return self
 
-    def actions(self, commands: Iterable[Tuple[Command, int]]) -> Ship:
+    def actions(self, commands: Iterable[Tuple[str, int]]) -> Ship:
         """Run the series of actions.
 
         >>> Ship().actions(test1).manhattan
@@ -119,32 +86,28 @@ class ShipWithWaypoint(Ship):
         self.wp_x = 10
         self.wp_y = 1
 
-    def action(self, com: Command, param: int) -> Ship:
+    def action(self, com: str, param: int) -> Ship:
         """Update the ship based on the action.
 
-        >>> Ship().action(Command.FORWARD, 10).x
-        10
+        >>> ShipWithWaypoint().action("F", 10).x
+        100
         """
-        if com == Command.NORTH:
+        if com == "N":
             self.wp_y += param
-        elif com == Command.EAST:
+        elif com == "E":
             self.wp_x += param
-        elif com == Command.SOUTH:
+        elif com == "S":
             self.wp_y -= param
-        elif com == Command.WEST:
+        elif com == "W":
             self.wp_x -= param
-        elif com == Command.FORWARD:
+        elif com == "F":
             self.x += self.wp_x * param
             self.y += self.wp_y * param
-        elif (com == Command.LEFT and param == 90) or (
-            com == Command.RIGHT and param == 270
-        ):
+        elif (com == "L" and param == 90) or (com == "R" and param == 270):
             self.wp_x, self.wp_y = -self.wp_y, self.wp_x
-        elif (com == Command.LEFT or com == Command.RIGHT) and param == 180:
+        elif (com == "L" or com == "R") and param == 180:
             self.wp_x, self.wp_y = -self.wp_x, -self.wp_y
-        elif (com == Command.RIGHT and param == 90) or (
-            com == Command.LEFT and param == 270
-        ):
+        elif (com == "R" and param == 90) or (com == "L" and param == 270):
             self.wp_x, self.wp_y = self.wp_y, -self.wp_x
         else:
             raise RuntimeError("Bad command for action", com, param)
@@ -153,6 +116,6 @@ class ShipWithWaypoint(Ship):
 
 if __name__ == "__main__":
     testmod()
-    commands: List[Tuple[Command, int]] = [parse_command(line) for line in stdin]
+    commands: List[Tuple[str, int]] = [(line[0], int(line[1:])) for line in stdin]
     print(Ship().actions(commands).manhattan)
     print(ShipWithWaypoint().actions(commands).manhattan)
