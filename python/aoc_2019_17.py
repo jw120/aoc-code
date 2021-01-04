b12583ba41a858e1d849b31b81d1e53a565a6413
Joe Watson <joe_watson@mail.com>
2021-01-04 14:04:47 +0800

    2019 day 17 done part one

--- python/aoc_2019_17.py
+++ python/aoc_2019_17.py
@@ -25,13 +25,13 @@ class Coord:
 
     def move(self, d: Direction) -> Coord:
         if d is Direction.UP:
-            return Coord(self.x, self.y - 1)
-        elif d is Direction.RIGHT:
-            return Coord(self.x + 1, self.y)
-        if d is Direction.DOWN:
-            return Coord(self.x, self.y + 1)
-        elif d is Direction.LEFT:
             return Coord(self.x - 1, self.y)
+        elif d is Direction.RIGHT:
+            return Coord(self.x, self.y + 1)
+        if d is Direction.DOWN:
+            return Coord(self.x + 1, self.y)
+        elif d is Direction.LEFT:
+            return Coord(self.x, self.y - 1)
         else:
             assert_never(d)
 
@@ -51,15 +51,23 @@ class Direction(Enum):
     def to_char(self) -> str:
         return "^>v<"[self.value]
 
+    def opposite(self) -> Direction:
+        return Direction((self.value + 2) % 4)
+
 
 class Scaffolding:
     def __init__(self, code: List[int]) -> None:
         self.m = Machine(code)
         self.scaffold: Set[Coord] = set()
-        self.robot_loc: Coord = Coord.origin()
-        self.robot_dir: Direction = Direction.UP
+        self.intersections: Set[Coord] = set()
+        self.robot_loc: Coord
+        self.robot_dir: Direction
         self.max: Coord = Coord.origin()
+        self._scan_image()
+        self._walk_scaffold()
 
+    def _scan_image(self) -> None:
+        """Find the robot and scaffold locations from IntCode machine output."""
         self.m.run()
         image: str = "".join(chr(c) for c in self.m.output_vals)
         for row, line in enumerate(image.split("\n")):
@@ -74,12 +82,39 @@ class Scaffolding:
             if row > self.max.x:
                 self.max = Coord(row, self.max.y)
 
+    def _walk_scaffold(self) -> int:
+        """Find intersections by walking the scaffold from the robot's location."""
+        current_loc: Coord = self.robot_loc
+        current_dir: Direction = self.robot_dir
+
+        while True:
+            available_dirs = [
+                d
+                for d in Direction
+                if current_loc.move(d) in self.scaffold and d != current_dir.opposite()
+            ]
+            if not available_dirs:  # Dead-end means we have finished
+                break
+            elif len(available_dirs) == 1:  # Just keep going
+                current_dir = available_dirs[0]
+            elif len(available_dirs) == 3:  # Cross-roads
+                self.intersections.add(current_loc)
+            else:
+                raise RuntimeError("Unexpected branch in scaffold")
+            current_loc = current_loc.move(current_dir)
+        return len(self.intersections)
+
+    def tally_intersections(self) -> int:
+        return sum(c.x * c.y for c in self.intersections)
+
     def show(self) -> None:
         for row in range(self.max.x):
             for col in range(self.max.y):
                 c: Coord = Coord(row, col)
                 if c == self.robot_loc:
                     print(self.robot_dir.to_char(), end="")
+                elif c in self.intersections:
+                    print("O", end="")
                 else:
                     print("#" if Coord(row, col) in self.scaffold else ".", end="")
             print()
@@ -88,4 +123,5 @@ class Scaffolding:
 if __name__ == "__main__":
     code = [int(x) for x in stdin.read().split(",")]
     scaffolding = Scaffolding(code)
+    print(scaffolding.tally_intersections())
     scaffolding.show()
