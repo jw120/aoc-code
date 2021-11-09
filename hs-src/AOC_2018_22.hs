7036f28417f854e00069f31153aa941c54afd326
Joe Watson <jw120@users.noreply.github.com>
2021-11-09 20:55:43 +0800

    more 22

--- hs-src/AOC_2018_22.hs
+++ hs-src/AOC_2018_22.hs
@@ -13,6 +13,8 @@ import Data.Array qualified as A (array, elems, (!))
 import Data.Ix qualified as Ix (range)
 import Data.Text (Text)
 import Data.Text qualified as T (breakOn, drop, lines, pack, stripPrefix)
+
+import Search (bfsVariable)
 import Utilities (readUnsignedDecimal)
 
 solvers :: Text -> (Text, Text)
@@ -24,37 +26,38 @@ solvers t = case T.lines t of
                 targetX = readUnsignedDecimal targetXText
                 targetY = readUnsignedDecimal $ T.drop 1 targetYText
              in ( T.pack . show $ riskLevel depth (targetX, targetY)
-                , "NYI"
+                , T.pack . show $ distance depth (targetX, targetY)
                 )
         _ -> error "Malformed input"
     _ -> error "Expected two lines for input"
 
 data RegionType = Rocky | Wet | Narrow
+newtype ErosionLevel = ErosionLevel Int
+newtype GeologicalIndex = GeologicalIndex Int
+type Position = (Int, Int)
+data Tool = Climbing | Torch | Neither deriving (Eq, Show)
+data State = State {statePos :: Position, stateTool :: Tool}
 
 -- | Convert erosion Level to region Type
-regionType :: Int -> RegionType
-regionType i = case i `mod` 3 of
+regionType :: ErosionLevel -> RegionType
+regionType (ErosionLevel i) = case i `mod` 3 of
     0 -> Rocky
     1 -> Wet
     _ -> Narrow
 
--- Convert region type to risk level
-regionRisk :: RegionType -> Int
-regionRisk Rocky = 0
-regionRisk Wet = 1
-regionRisk Narrow = 2
+-- | Convert geological index to erosion level
+erosionLevel :: Int -> GeologicalIndex -> ErosionLevel
+erosionLevel depth (GeologicalIndex g) = ErosionLevel $ (depth + g) `mod` 20183
 
--- | Convert geologic index to erosion level
-erosionLevel :: Int -> Int -> Int
-erosionLevel depth geologicalIndex = (depth + geologicalIndex) `mod` 20183
+-- | Geologic indices of regions at depth from (0,0 ) to target (x, y)
+geologicIndex :: Int -> Position -> Position -> GeologicIndex
+geologicIndex depth target pos = (geologicIndices depth target) A.! pos
 
 -- | Geologic indices of regions at depth from (0,0 ) to target (x, y)
 geologicIndices :: Int -> (Int, Int) -> Array (Int, Int) Int
-geologicIndices depth (xMax, yMax) = arr
+geologicIndices depth (xMax, yMax) = A.array xyRange [(c, g c) | c <- Ix.range xyRange]
   where
-    arr :: Array (Int, Int) Int
-    arr = A.array xyRange [(c, g c) | c <- Ix.range xyRange]
-    xyRange = ((0, 0), (xMax, yMax))
+    xyRange = ((0, 0), (2 * xMax, 2 * yMax))
     g :: (Int, Int) -> Int
     g (0, 0) = 0
     g (x, 0) = x * 16807
@@ -63,8 +66,36 @@ geologicIndices depth (xMax, yMax) = arr
         | x == xMax && y == yMax = 0
         | otherwise = erosionLevel depth (arr A.! (x - 1, y)) * erosionLevel depth (arr A.! (x, y - 1))
 
--- | Risk level of area with given depth from (0, 0) to target (x, y)
+-- | Part (a): Sum of risk levels from (0, 0) to target (x, y)
 riskLevel :: Int -> (Int, Int) -> Int
 riskLevel depth (xMax, yMax) = sum regionTypes
   where
     regionTypes = map (regionRisk . regionType . erosionLevel depth) . A.elems $ geologicIndices depth (xMax, yMax)
+    regionRisk :: RegionType -> Int
+    regionRisk Rocky = RiskLevel 0
+    regionRisk Wet = 1
+    regionRisk Narrow = 2
+
+-- Return up/down/left/right positions
+adjacents :: Position -> [Position]
+adjacents (x, y) = [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
+
+-- Which tool is invalid at the given position
+invalidTool :: Int -> Position -> Tool
+invalidTool = case regionType (erosionLevel depth pos) of
+    Rocky -> Neither
+    Wet -> Torch
+    Narrow -> Climbing
+
+-- Distance from origin to given location using tool switching
+distance :: Int -> (Int, Int) -> Int
+distance depth target = fst . bfsVariable moves 7 targetState startState
+  where
+    stateState = State{statePos = (0, 0), stateTool = Torch}
+    targetState = State{statePos = target, stateTool = Torch}
+    moves :: Int -> State -> [State]
+    moves 1 State{statePos = pos, stateTool = tool} =
+        filter ((/= tool) . invalidTool depth) $ adjacents pos
+    moves 7 State{statePos = pos, stateTool = tool} =
+        filter (/= invalidTool depth pos) $ filter (/= tool) [Climbing, Torch, Neither]
+    moves _ = []
