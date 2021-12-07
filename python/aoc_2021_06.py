c8cdb4460a7106ef927a11f8fec8077525a611b7
Joe Watson <jw120@users.noreply.github.com>
2021-12-07 12:37:31 +0800

    2021 6 done in python

---
+++ python/aoc_2021_06.py
@@ -0,0 +1,29 @@
+"""Advent of Code 2021 - Day 6."""
+
+from doctest import testmod
+from sys import stdin
+from typing import Dict
+
+test_data: list[int] = [3, 4, 3, 1, 2]
+
+
+def fish(initial_timers: list[int], number_of_steps: int) -> int:
+    """Return number of fish after given number of steps from initial position.
+
+    >>> [fish(test_data, i) for i in [1, 2, 18, 256]]
+    [5, 6, 26, 26984457539]
+    """
+    f: Dict[int, int] = {}  # number of fish after n steps from one timer-0 fish
+    for i in range(-8, number_of_steps):
+        if i <= 0:
+            f[i] = 1
+        else:
+            f[i] = f[i - 1 - 8] + f[i - 1 - 6]
+    return sum(f[number_of_steps - x] for x in initial_timers)
+
+
+if __name__ == "__main__":
+    testmod()
+    initial: list[int] = [int(x) for x in stdin.read().split(",")]
+    print(fish(initial, 80))
+    print(fish(initial, 256))
