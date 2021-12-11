9cac34d514a486a7ff8b274ea697386da032f3b4
Joe Watson <jw120@users.noreply.github.com>
2021-12-11 14:00:27 +0800

    2021 10 done

--- python/aoc_2021_10.py
+++ python/aoc_2021_10.py
@@ -2,56 +2,77 @@
 
 from collections import deque
 from doctest import testmod
+from enum import Enum, auto
 from sys import stdin
-from typing import Dict, Optional
+from typing import Dict, Set, Tuple
 
 
-test: list[str] = [
-    "[({(<(())[]>[[{[]{<()<>>",
-    "[(()[<>])]({[<{<<[]>>(",
-    "{([(<{}[<>[]}>{[]{[(<()>",
-    "(((({<>}<{<{<>}{[]{[]{}",
-    "[[<[([]))<([[{}[[()]]]",
-    "[{[{({}]{}}([{[{{{}}([]",
-    "{<[[]]>}<{[{[{[]{()[[[]",
-    "[<(<(<(<{}))><([]([]()",
-    "<{([([[(<>()){}]>(<<{{",
-    "<{([{{}}[<[[[<>{}]]]>[]]",
+class Result(Enum):
+    Illegal = auto()
+    Incomplete = auto()
+
+
+test: list[Tuple[str, Tuple[Result, str]]] = [
+    ("[({(<(())[]>[[{[]{<()<>>", (Result.Incomplete, "}}]])})]")),
+    ("[(()[<>])]({[<{<<[]>>(", (Result.Incomplete, ")}>]})")),
+    ("{([(<{}[<>[]}>{[]{[(<()>", (Result.Illegal, "}")),
+    ("(((({<>}<{<{<>}{[]{[]{}", (Result.Incomplete, "}}>}>))))")),
+    ("[[<[([]))<([[{}[[()]]]", (Result.Illegal, ")")),
+    ("[{[{({}]{}}([{[{{{}}([]", (Result.Illegal, "]")),
+    ("{<[[]]>}<{[{[{[]{()[[[]", (Result.Incomplete, "]]}}]}]}>")),
+    ("[<(<(<(<{}))><([]([]()", (Result.Illegal, ")")),
+    ("<{([([[(<>()){}]>(<<{{", (Result.Illegal, ">")),
+    ("<{([{{}}[<[[[<>{}]]]>[]]", (Result.Incomplete, "])}>")),
 ]
 
-
 pairs: Dict[str, str] = {"(": ")", "[": "]", "{": "}", "<": ">"}
+closes: Set[str] = set(pairs.values())
+illegal_scores: Dict[str, int] = {")": 3, "]": 57, "}": 1197, ">": 25137}
+completion_scores: Dict[str, int] = {")": 1, "]": 2, "}": 3, ">": 4}
 
 
-def first_illegal(s: str) -> Optional[str]:
-    """Return the first illegal character, or None if pattern is incomplete.
+def scan(s: str) -> Tuple[Result, str]:
+    """Return Illegal and the first illegal character or Incomplete and the completion.
 
-    >>> [first_illegal(s) for s in test]
-    [None, None, '}', None, ')', ']', None, ')', '>', None]
+    >>> [scan(s) == r for s, r in test]
+    [True, True, True, True, True, True, True, True, True, True]
     """
     stack: deque[str] = deque()
     for c in s:
         if c in pairs:
             stack.append(c)
-        elif not stack:
-            return None
-        elif c != pairs[stack.pop()]:
-            return c
-    return None
+        elif not stack or c not in closes or c != pairs[stack.pop()]:
+            return (Result.Illegal, c)
+    stack.reverse()
+    return (Result.Incomplete, "".join(pairs[c] for c in stack))
 
 
-scores: Dict[str, int] = {")": 3, "]": 57, "}": 1197, ">": 25137}
-
-
-def score(c: str) -> int:
+def illegal_score(c: str) -> int:
     """Return score of the illegal character."""
     try:
-        return scores[c]
+        return illegal_scores[c]
     except KeyError:
         raise ValueError("Bad illegal character: " + c)
 
 
+def completion_score(s: str) -> int:
+    """Return score of the completion string.
+
+    >>> [completion_score(c) for _, (r, c) in test if r == Result.Incomplete]
+    [288957, 5566, 1480781, 995444, 294]
+    """
+    score = 0
+    for c in s:
+        score = score * 5 + completion_scores[c]
+    return score
+
+
 if __name__ == "__main__":
     testmod()
-    lines = stdin.read().splitlines()
-    print(sum(score(p) for p in (first_illegal(s) for s in lines) if p is not None))
+    lines: list[str] = stdin.read().splitlines()
+    scans: list[Tuple[Result, str]] = [scan(s) for s in lines]
+    print(sum(illegal_score(c) for r, c in scans if r == Result.Illegal))
+    scores: list[int] = [
+        completion_score(s) for r, s in scans if r == Result.Incomplete
+    ]
+    print(sorted(scores)[len(scores) // 2])
