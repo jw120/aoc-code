db3f2eb27d1ad957b900615a0526cbd7ac0a0eb4
Joe Watson <jw120@users.noreply.github.com>
2021-12-19 17:40:50 +0800

    work on snails

--- hs-src/AOC_2021_18.hs
+++ hs-src/AOC_2021_18.hs
@@ -6,92 +6,82 @@
  Maintainer  : jw1200@gmail.com
  Stability   : experimental
 -}
-module AOC_2021_18 (solvers, add, addList, magnitude, readSnail, Snail (..)) where
+module AOC_2021_18 (solvers, add, addList, magnitude, readSnail, reduce, showSnail) where
 
+import Data.Char qualified as C (isDigit)
 import Data.List qualified as L (foldl1')
 import Data.Text (Text)
-import Data.Text qualified as T (lines, pack)
-
-import Text.Megaparsec.Char qualified as MC (char)
-
-import Utilities (Parser, pUnsignedInt, parseOrStop, (<|>))
+import Data.Text qualified as T (lines, pack, unpack)
 
 solvers :: Text -> (Text, Text)
 solvers t =
-    ( T.pack . show . magnitude $ numbers
+    ( T.pack . show . magnitude $ addList numbers
     , "NYI" -- T.pack . show $ eval packet
     )
   where
     numbers = map readSnail $ T.lines t
 
-data Snail
-    = SnailReg Int -- A regular number
-    | SnailPair Snail Snail -- A pair
+data SnailTerm = Open | Close | Comma | Reg Int
 
-addList :: [Snail] -> Snail
+type SnailNumber = [SnailTerm]
+
+addList :: [SnailNumber] -> SnailNumber
 addList = L.foldl1' add
 
-add :: Snail -> Snail -> Snail
-add (SnailPair m a b) (SnailPair n p q) = reduce $ SnailPair 0 (SnailPair (m + 1) a b) (SnailPair (n + 1) p q)
-add _ _ = error "Can only add two SnailPairs"
+add :: SnailNumber -> SnailNumber -> SnailNumber
+add x y = reduce $ [Open] ++ x ++ [Comma] ++ y ++ [Close]
 
+-- Apply explosions and split until no more possible
+reduce :: SnailNumber -> SnailNumber
+reduce xs = maybe xs reduce $ reduce' 0 [] xs
 
-
-reduce :: Int -> [Snail] -> [Snail] -> Maybe [Snail]
+-- Apply first explosion or split to the list. Nothing if no reduction to make
+reduce' :: Int -> SnailNumber -> SnailNumber -> Maybe SnailNumber
 -- Explode a pair
-reduce 4 left (Reg x: Reg y : right) = Just $ addToLast x left ++ [Reg 0] ++ addToFirst y right
-reduce n left (Reg x: right)
-    -- Split a number
-    | x > 10 = Just $ left ++ [Reg x `div` 2, Reg x `div` 2 + if odd x then 1 else 0] ++ right
-    | otherwise = reduce n (left ++ [Reg x]) right
-reduce n left (Open : right) = reduce (n + 1) (left ++ [Open]) right
-reduce n left (Close : right) = reduce (n - 1) (left ++ [Close]) right
-reduce _ _ [] = Nothing
+reduce' n left right = case (n, right) of
+    (4, Open : Reg x : Comma : Reg y : Close : rest) -> Just $ addToLast x left ++ [Reg 0] ++ addToFirst y rest
+    (_, Reg x : rest) ->
+        if x > 10
+            then Just $ left ++ [Open, Reg (x `div` 2), Comma, Reg (x `div` 2 + if odd x then 1 else 0), Close] ++ rest
+            else reduce' n (left ++ [Reg x]) rest
+    (_, Open : rest) -> reduce' (n + 1) (left ++ [Open]) rest
+    (_, Close : rest) -> reduce' (n - 1) (left ++ [Close]) rest
+    (_, Comma : rest) -> reduce' n (left ++ [Comma]) rest
+    (_, []) -> Nothing
 
+-- Add z to the first regular term in the sequence
+addToFirst :: Int -> SnailNumber -> SnailNumber
+addToFirst z (Reg x : rest) = Reg (x + z) : rest
+addToFirst z (y : rest) = y : addToFirst z rest
+addToFirst _ [] = []
 
+addToLast :: Int -> SnailNumber -> SnailNumber
+addToLast z = reverse . addToFirst z . reverse
 
-fromLeft :: Int -> Int -> Seq Snail -> Seq Snail -> Maybe (Seq Snail)
-fromLeft level 0 left (Reg x :|> right) = fromLeft level 0 left' right'
-    where
-        left' = fromRight left
-fromLeft level explodeValue left (Reg x :|> right)
-    | explodeValue + x > 10 = let left' =
-        fromLeft levels 0 left' right'
+magnitude :: SnailNumber -> Int
+magnitude = const 0
 
+-- magnitude (SnailReg x) = x
+-- magnitude (SnailPair _ p q) = 3 * magnitude p + 2 * magnitude q
 
-
-
-
-reduce :: Snail -> Snail
-reduce = fst . reduce' 0 0
+readSnail :: Text -> SnailNumber
+readSnail = go . T.unpack
   where
-    -- inputs are surrounding pairs and exploding value coming in, output includes exploding value coming out
-    reduce' :: Int -> Int -> Snail -> (Snail, Int)
-    reduce' n _x (SnailReg i)
-        | i < 10 = (SnailReg i, 0)
-        | otherwise = (SnailPair n (i `div` 2) (i `div` 2 + if odd i then 1 else 0), 0)
-    reduce' 4 x (SnailPair (SnailReg i) (SnailReg j)) = (SnailReg 0, i, j)
-    reduce' n left (SnailPair p q)
-        | n < 4 =
-            let (p', pLeft, pRight) = reduce' (n + 1) left p
-                (q', qLeft, qRight) = reduce' (n + 1) pRight q
-             in (SnailPair p' q', x'')
-        | n == 4 =
+    go ('[' : xs) = Open : go xs
+    go (']' : xs) = Close : go xs
+    go (',' : xs) = Comma : go xs
+    go (x : xs)
+        | C.isDigit x =
+            let (num, rest) = span C.isDigit (x : xs)
+             in Reg (read num) : go rest
+        | otherwise = error "Unexpected character"
+    go [] = []
 
-magnitude :: Snail -> Int
-magnitude (SnailReg x) = x
-magnitude (SnailPair _ p q) = 3 * magnitude p + 2 * magnitude q
-
-readSnail :: Text -> Snail
-readSnail = parseOrStop (pSnail 0)
+showSnail :: SnailNumber -> String
+showSnail = concatMap showTerm
   where
-    pSnail :: Int -> Parser Snail
-    pSnail n = pReg <|> pPair n
-    pPair n = do
-        _ <- MC.char '['
-        x <- pSnail (n + 1)
-        _ <- MC.char ','
-        y <- pSnail (n + 1)
-        _ <- MC.char ']'
-        return $ SnailPair n x y
-    pReg = SnailReg <$> pUnsignedInt
+    showTerm :: SnailTerm -> String
+    showTerm Open = "["
+    showTerm Close = "]"
+    showTerm Comma = ","
+    showTerm (Reg x) = show x
--- hs-test/AOC_2021_18_Test.hs
+++ hs-test/AOC_2021_18_Test.hs
@@ -2,7 +2,63 @@ module AOC_2021_18_Test where
 
 import Test.Tasty.HUnit ((@?=))
 
-import AOC_2021_18 (magnitude, readSnail)
+import AOC_2021_18 (add, addList, magnitude, readSnail, reduce, showSnail)
+
+unit_2021_18_snail_reduce1 :: IO ()
+unit_2021_18_snail_reduce1 = showSnail (reduce (readSnail "[[[[[9,8],1],2],3],4]")) @?= "[[[[0,9],2],3],4]"
+
+unit_2021_18_snail_reduce2 :: IO ()
+unit_2021_18_snail_reduce2 = showSnail (reduce (readSnail "[7,[6,[5,[4,[3,2]]]]]")) @?= "[7,[6,[5,[7,0]]]]"
+
+unit_2021_18_snail_reduce3 :: IO ()
+unit_2021_18_snail_reduce3 = showSnail (reduce (readSnail "[[6,[5,[4,[3,2]]]],1]")) @?= "[[6,[5,[7,0]]],3]"
+
+unit_2021_18_snail_reduce4 :: IO ()
+unit_2021_18_snail_reduce4 = showSnail (reduce (readSnail "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]")) @?= "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"
+
+unit_2021_18_snail_reduce5 :: IO ()
+unit_2021_18_snail_reduce5 = showSnail (reduce (readSnail "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")) @?= "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"
+
+unit_2021_18_snail_add1 :: IO ()
+unit_2021_18_snail_add1 =
+    showSnail (add (readSnail "[[[[4,3],4],4],[7,[[8,4],9]]]") (readSnail "[1,1]")) @?= "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
+
+unit_2021_18_snail_add_list1 :: IO ()
+unit_2021_18_snail_add_list1 =
+    let xs = map readSnail ["[1,1]", "[2,2]", "[3,3]", "[4,4]"]
+        ans = "[[[[1,1],[2,2]],[3,3]],[4,4]]"
+     in showSnail (addList xs) @?= ans
+
+unit_2021_18_snail_add_list2 :: IO ()
+unit_2021_18_snail_add_list2 =
+    let xs = map readSnail ["[1,1]", "[2,2]", "[3,3]", "[4,4]", "[5,5]"]
+        ans = "[[[[3,0],[5,3]],[4,4]],[5,5]]"
+     in showSnail (addList xs) @?= ans
+
+unit_2021_18_snail_add_list3 :: IO ()
+unit_2021_18_snail_add_list3 =
+    let xs = map readSnail ["[1,1]", "[2,2]", "[3,3]", "[4,4]", "[5,5]", "[6,6]"]
+        ans = "[[[[5,0],[7,4]],[5,5]],[6,6]]"
+     in showSnail (addList xs) @?= ans
+
+unit_2021_18_snail_add_list4 :: IO ()
+unit_2021_18_snail_add_list4 =
+    let xs =
+            map
+                readSnail
+                [ "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]"
+                , "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]"
+                , "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]"
+                , "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]"
+                , "[7,[5,[[3,8],[1,4]]]]"
+                , "[[2,[2,2]],[8,[8,1]]]"
+                , "[2,9]"
+                , "[1,[[[9,3],9],[[9,0],[0,7]]]]"
+                , "[[[5,[7,4]],7],1]"
+                , "[[[[4,2],2],6],[8,7]]"
+                ]
+        ans = "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"
+     in showSnail (addList xs) @?= ans
 
 unit_2021_18_magnitude1 :: IO ()
 unit_2021_18_magnitude1 = magnitude (readSnail "[9,1]") @?= 29
