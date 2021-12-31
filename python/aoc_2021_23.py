6d0cfad354939aee820878638dcd6575d446e67b
Joe Watson <jw120@users.noreply.github.com>
2021-12-31 11:57:01 +0900

    working again 2021 23 with size 2

--- python/aoc_2021_23.py
+++ python/aoc_2021_23.py
@@ -4,10 +4,9 @@ from __future__ import annotations
 
 from collections import Counter
 from doctest import testmod
+from heapq import heappop, heappush
 from sys import stdin
 from typing import Any, ClassVar, Iterable, Optional, Tuple
-
-# from heapq import heappop, heappush
 
 
 class Kind:
@@ -72,6 +71,9 @@ class Amphipod:
             and self.index == other.index
         )
 
+    def __hash__(self) -> int:
+        return hash(self.index)
+
     def __str__(self) -> str:
         return self.kind.char + "_" + str(self.index)
 
@@ -89,7 +91,7 @@ class Amphipod:
 class Position:
 
     hallway_size: ClassVar[int] = 11
-    valid_hallway_indices: ClassVar[set[int]] = {0, 1, 3, 5, 7, 9, 10}
+    valid_hallway_indices: ClassVar[list[int]] = [0, 1, 3, 5, 7, 9, 10]
 
     def __init__(self, room: Optional[Kind], room_index: int) -> None:
         self.room = room
@@ -134,6 +136,7 @@ class State:
         """
         self.room_size = room_size
         self.mapping: dict[Position, Amphipod] = {}
+        self.cost: int = 0  # Used only within .solve()
         if s is not None:
             Amphipod.reset()
             assert len(s) == len(
@@ -152,21 +155,27 @@ class State:
                     )
             assert len(self.mapping) == 4 * room_size, "Wrong number of items in state"
 
-    def room_available(self, target_kind: Kind) -> bool:
-        """Is the target room available for an amphipod to move into.
+    def room_available(self, target_kind: Kind) -> Optional[Position]:
+        """If the target room available for an amphipod to move into, return the open position.
 
         Available if only occupied by amphipods of the same kind.
 
         >>> [test1.room_available(kind) for kind in Kind.every()]
-        [False, False, False, False]
-
+        [None, None, None, None]
         >>> [test2.room_available(kind) for kind in Kind.every()]
-        [False, False, True, False]
+        [None, None, Position('C', 0), None]
+        >>> [test3.room_available(kind) for kind in Kind.every() if kind != Kind('C')]
+        [None, Position('B', 1), None]
         """
+        # Target room is available if only occupied by pods of target kind
         for position, pod in self.mapping.items():
             if position.room == target_kind and pod.kind != target_kind:
-                return False
-        return True
+                return None
+        # Return highest index open space
+        for i in range(self.room_size - 1, -1, -1):
+            if (p := Position(target_kind, i)) not in self.mapping:
+                return p
+        raise RuntimeError("Unexpected request for room_available when room complete")
 
     @property
     def hallway_number(self) -> int:
@@ -178,9 +187,10 @@ class State:
         return sum(position.room is None for position in self.mapping.keys())
 
     def path_available(self, p: Position, q: Position) -> bool:
-        """Is a path available between the room and hallway (no blocking amphipods).
+        """Is a path available between two positions (no blocking amphipods).
 
-        Does not test if either end-state is occupied.
+        Does not test if either end-state is occupied. Only considers paths from a room to a hallway
+        (or vice versa).
 
         >>> [test1.path_available(Position(r, i), Position(None, 0)) for r in Kind.every() for i in range(2)]
         [True, False, True, False, True, False, True, False]
@@ -190,9 +200,14 @@ class State:
         [False, False, True, False, True, True, True, False]
         >>> [test3.path_available(Position(r, i), Position(None, 3)) for r in Kind.every() for i in range(2)]
         [True, False, True, True, False, False, False, False]
+        >>> [test3.path_available(Position(None, 3), Position(r, i)) for r in Kind.every() for i in range(2)]
+        [True, False, True, True, False, False, False, False]
         """
-        assert p.room is not None, "path_available start must be a room"
-        assert q.room is None, "path_available destination must be a hallway"
+        if p.room is None:
+            p, q = q, p
+        assert p.room is not None, "path_available cannot be hallway to hallway"
+        assert q.room is None, "path_available cannot be room to room"
+        # Now we have p is a room position and q is a hallway position
         # Check to see if blocked by upper position in the room
         if any(Position(p.room, i) in self.mapping for i in range(p.index)):
             return False
@@ -209,53 +224,52 @@ class State:
         """Return all available moves.
 
         >>> expected_moves = {(Position(k, 0), Position(None, h)) for k in Kind.every() for h in Position.valid_hallway_indices}
-        >>> expected_moves
-        QQ
-        >>> list(test1.available_moves(3))
-        QQQ
         >>> set(test1.available_moves(3)) == expected_moves
         True
-        # >>> expected_moves =  {(('A', True),  h) for h in [0,1]}
-        # >>> expected_moves |= {(('B', True),  h) for h in [5, 7, 9, 10]}
-        # >>> expected_moves |= {(('D', True),  h) for h in [5, 7, 9, 10]}
-        # >>> set(available_moves(test2, 3)) == expected_moves
-        # True
-        # >>> expected_moves = {(3, ('B', False))}
-        # >>> expected_moves |= {(('A', True),  h) for h in [0,1]}
-        # >>> expected_moves |= {(('D', True),  h) for h in [7, 9, 10]}
-        # >>> set(available_moves(test3, 3)) == expected_moves
-        # True
+        >>> expected_moves =  {(Position(Kind('A'), 0),  Position(None, h)) for h in [0,1]}
+        >>> expected_moves |= {(Position(Kind('B'), 0),  Position(None, h)) for h in [5, 7, 9, 10]}
+        >>> expected_moves |= {(Position(Kind('D'), 0),  Position(None, h)) for h in [5, 7, 9, 10]}
+        >>> set(test2.available_moves(3)) == expected_moves
+        True
+        >>> expected_moves = {(Position(None, 3), Position(Kind('B'), 1))}
+        >>> expected_moves |= {(Position(Kind('A'), 0),  Position(None, h)) for h in [0,1]}
+        >>> expected_moves |= {(Position(Kind('D'), 0),  Position(None, h)) for h in [7, 9, 10]}
+        >>> set(test3.available_moves(3)) == expected_moves
+        True
         """
-        # for start_position, pod in state.items():
-        #     kind = pod_kind(pod)
-        #     # Amphipod is in the hallway, can only move to a room
-        #     if isinstance(start_position, int):
-        #         if room_available(state, kind):
-        #             end_room: RoomPosition = (kind, not room_empty(state, kind))
-        #             if path_available(state, end_room, start_position):
-        #                 yield (start_position, end_room)
-        #     # Amphipod in room moves to hallway
-        #     else:
-        #         if hallway_number(state) < max_hallway_number:
-        #             start_kind, start_upper = start_position
-        #             # Only try and move if pod is in the wrong room (or in the right room but above a wrong pod)
-        #             if start_kind != kind or (
-        #                 start_upper and pod_kind(state[(start_kind, False)]) != kind
-        #             ):
-        #                 for end_hall in valid_hallway_positions:
-        #                     if end_hall not in state and path_available(
-        #                         state, start_position, end_hall
-        #                     ):
-        #                         yield (start_position, end_hall)
-        pass
+        for start, pod in self.mapping.items():
+            # Amphipod is in the hallway, can only move to the room of its own kind
+            if start.room is None:
+                end = self.room_available(pod.kind)
+                if end is not None and self.path_available(start, end):
+                    yield (start, end)
+            # Amphipod in room moves to hallway
+            else:
+                if self.hallway_number < max_hallway_number:
+                    # Only try and move if pod is in the wrong room or in the right room but above a wrong pod
+                    if start.room != pod.kind or any(
+                        self.mapping.get(p, pod).kind != pod.kind
+                        for p in (
+                            Position(start.room, i)
+                            for i in range(start.index + 1, self.room_size)
+                        )
+                    ):
+                        for end in (
+                            Position(None, i) for i in Position.valid_hallway_indices
+                        ):
+                            if end not in self.mapping and self.path_available(
+                                start, end
+                            ):
+                                yield (start, end)
 
     def copy(self) -> State:
         """Return a copy of the state."""
         other = State(self.room_size, None)
         other.mapping = self.mapping.copy()
+        other.cost = self.cost
         return other
 
-    def move(self, p: Position, q: Position) -> Tuple[State, int]:
+    def move(self, p: Position, q: Position) -> State:
         """Return a new state applying the given move and the cost of the move."""
         assert p in self.mapping, "Can't move from an empty position"
         pod = self.mapping[p]
@@ -266,18 +280,19 @@ class State:
             if x.room is None:
                 return x.index, 0
             else:
-                return x.room.hallway_exit, x.index
+                return x.room.hallway_exit, x.index + 1
 
         p_hallway, p_extra = hallway_and_extra(p)
         q_hallway, q_extra = hallway_and_extra(q)
         distance = abs(p_hallway - q_hallway) + p_extra + q_extra
-        cost = distance * pod.kind.cost
+        extra_cost = distance * pod.kind.cost
 
         new_state = self.copy()
         new_state.mapping[q] = pod
+        new_state.cost = self.cost + extra_cost
         del new_state.mapping[p]
 
-        return new_state, cost
+        return new_state
 
     @property
     def organized(self) -> bool:
@@ -309,58 +324,73 @@ class State:
                 row += pod.kind.char if pod is not None else "-"
             print(" ", " ".join(row))
 
-
-StateHashable = frozenset[Tuple[Position, Amphipod]]
-
-
-class WrappedState:
-    def __init__(
-        self, state_hashable: StateHashable, cost_lookup: dict[StateHashable, int]
-    ) -> None:
-        self.state_hashable = state_hashable
-        self.cost_lookup = cost_lookup
-
-    def cost(self) -> int:
-        return self.cost_lookup[self.state_hashable]
-
     def __hash__(self) -> int:
-        return hash(self.state_hashable)
+        """Hash provided to allow set and dict lookup - based only on mapping."""
+        return hash(frozenset(self.mapping.items()))
+
+    def __eq__(self, other: Any) -> bool:
+        """Hash provided to allow set and dict lookup - based only on mapping."""
+        return isinstance(other, State) and self.mapping == other.mapping
+
+
+class QueuedState:
+    """Wrapped version of state with an ordering provided to allow use in a priority queue based only on cost."""
+
+    def __init__(self, state: State):
+        self.state = state
 
     def __lt__(self, other: Any) -> bool:
-        if isinstance(other, WrappedState):
-            return self.cost() < other.cost()
+        if isinstance(other, QueuedState):
+            return self.state.cost < other.state.cost
         return NotImplemented
 
 
 def solve(start_state: State, max_hallway_number: int) -> int:
-    """Solve the state (BFS a la Wikipedia).
+    """Solve the state.
 
     >>> solve(test1, 3)
     12521
     """
-    # start_state_hashable = frozenset(start_state.items())
-    # pq: Any = []  # heapq is an untyped module
-    # explored: dict[StateHashable, int] = {start_state_hashable: 0}
-    # heappush(pq, WrappedState(start_state_hashable, explored))
-    # while pq:
-    #     wrapped_state = heappop(pq)
-    #     state = dict(wrapped_state.state_hashable)
-    #     cost = explored[wrapped_state.state_hashable]
-    #     if organized(state):
-    #         # print("Found solution", cost)
-    #         # show_state(state)
-    #         return cost
-    #     for from_position, to_position in available_moves(state, max_hallway_number):
-    #         new_state, extra_cost = move(state, from_position, to_position)
-    #         new_state_hash = frozenset(new_state.items())
-    #         new_cost = cost + extra_cost
-    #         if new_state_hash in explored:
-    #             if new_cost < explored[new_state_hash]:
-    #                 explored[new_state_hash] = new_cost
-    #         else:
-    #             explored[new_state_hash] = new_cost
-    #             heappush(pq, WrappedState(new_state_hash, explored))
-    # raise RuntimeError("No solution found")
+    # Keep track of states to be explored in a priority queue of Tuple[State, Cost]
+    # Complication is that we may find a lower cost to the state after it has been queued, to
+    # take care of that we also keep track of the costs of queued items in a dictionary
+    # and adjust the pq when we find a state with a lower cost.
+    # Also note we used 'Any' as heapq is an untyped module
+    pq: Any = []  # heapq is an untyped module
+    heappush(pq, QueuedState(start_state))
+    # Costs of everything in the pq
+    pq_costs: dict[State, int] = {start_state: 0}
+    # We also keep track of all states either visited or in the queue
+    explored: set[State] = {start_state}
+    while pq:
+        queued_state: QueuedState = heappop(pq)
+        state = queued_state.state
+        del pq_costs[state]
+        # print("Cost", state.cost, "remaining", len(pq))
+        # state.show()
+        # print()
+        if state.organized:
+            # print("Found solution", cost)
+            # show_state(state)
+            return state.cost
+        for from_position, to_position in state.available_moves(max_hallway_number):
+            new_state = state.move(from_position, to_position)
+            if new_state not in explored:  # First time seeing this state
+                heappush(pq, QueuedState(new_state))
+                assert new_state not in pq_costs
+                pq_costs[new_state] = new_state.cost
+                explored.add(new_state)
+            elif new_state in pq_costs and new_state.cost < pq_costs[new_state]:
+                # State already in priority queue but now found with a lower cost
+                pq_removed: list[QueuedState] = []
+                while (qs := heappop(pq)).state != new_state:
+                    pq_removed.append(qs)
+                # print("Re-worked", len(pq_removed))
+                heappush(pq, QueuedState(new_state))
+                for qs in pq_removed:
+                    heappush(pq, qs)
+                pq_costs[new_state] = new_state.cost
+    raise RuntimeError("No solution found")
 
 
 """ test1 is the starting state for the example problem
@@ -385,7 +415,7 @@ test1: State = State(
   #########
 
 """
-test2: State = test1.move(Position(Kind("C"), 0), Position(None, 3))[0]
+test2: State = test1.move(Position(Kind("C"), 0), Position(None, 3))
 
 """ test3 is an intermediate step shown from the example problem
 
@@ -422,40 +452,40 @@ test_organized.mapping = dict(
 )
 
 
-# class TestMove:
-#     def __init__(self) -> None:
-#         self.state = test1
-#         self.total_cost = 0
+class TestMove:
+    def __init__(self, max_hallway_number: int) -> None:
+        self.state = test1
+        self.max_hallway_number = max_hallway_number
 
-#     def move(self, a: Position, b: Position) -> TestMove:
-#         assert (a, b) in available_moves(self.state)
-#         self.state, extra_cost = move(self.state, a, b)
-#         self.total_cost += extra_cost
-#         # print(self.total_cost)
-#         # show_state(self.state)
-#         return self
+    def move(self, a: Position, b: Position) -> TestMove:
+        assert (a, b) in self.state.available_moves(self.max_hallway_number)
+        self.state = self.state.move(a, b)
+        print(self.state.cost)
+        self.state.show()
+        return self
 
 
+# test1.show()
 # t = (
-#     TestMove()
-#     .move(("C", True), 3)
-#     .move(("B", True), 5)
-#     .move(5, ("C", True))
-#     .move(("B", False), 5)
-#     .move(3, ("B", False))
-#     .move(("A", True), 3)
-#     .move(3, ("B", True))
-#     .move(("D", True), 7)
-#     .move(("D", False), 9)
-#     .move(7, ("D", False))
-#     .move(5, ("D", True))
-#     .move(9, ("A", True))
+#     TestMove(3)
+#     .move(Position(Kind("C"), 0), Position(None, 3))
+#     .move(Position(Kind("B"), 0), Position(None, 5))
+#     .move(Position(None, 5), Position(Kind("C"), 0))
+#     .move(Position(Kind("B"), 1), Position(None, 5))
+#     .move(Position(None, 3), Position(Kind("B"), 1))
+#     .move(Position(Kind("A"), 0), Position(None, 3))
+#     .move(Position(None, 3), Position(Kind("B"), 0))
+#     .move(Position(Kind("D"), 0), Position(None, 7))
+#     .move(Position(Kind("D"), 1), Position(None, 9))
+#     .move(Position(None, 7), Position(Kind("D"), 1))
+#     .move(Position(None, 5), Position(Kind("D"), 0))
+#     .move(Position(None, 9), Position(Kind("A"), 0))
 # )
-# assert organized(t.state)
-# assert t.total_cost == 12521
+# assert t.state.organized
+# assert t.state.cost == 12521
 
 
 if __name__ == "__main__":
     testmod()
-#    state = State(2, stdin.read().strip())
-#    print(state.solve(3))
+    state = State(2, stdin.read().strip())
+    print(solve(state, 3))
