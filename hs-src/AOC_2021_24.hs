ee058eb108c792ee22b8ec6da999d3ffde3a08e4
Joe Watson <jw120@users.noreply.github.com>
2022-01-01 17:22:55 +0900

    Work on 2021 24

--- hs-src/AOC_2021_24.hs
+++ hs-src/AOC_2021_24.hs
@@ -6,7 +6,7 @@
  Maintainer  : jw1200@gmail.com
  Stability   : experimental
 -}
-module AOC_2021_24 (solvers, pInstruction, compile) where
+module AOC_2021_24 (solvers, parse, unparse, run) where
 
 import Data.List qualified as L (foldl')
 import Data.Text (Text)
@@ -14,7 +14,9 @@ import Data.Text (Text)
 --import Data.Text qualified as T (lines, pack, unpack)
 import Text.Megaparsec.Char qualified as MC (string)
 
-import Utilities (Parser, pSignedInt, ($>), (<|>)) -- parseOrStop,
+-- parseOrStop,
+import AOC_2018_19 (initialState)
+import Utilities (Parser, pSignedInt, parseOrStop, ($>), (<|>))
 
 solvers :: Text -> (Text, Text)
 solvers _t =
@@ -22,88 +24,113 @@ solvers _t =
     , "NYI" -- T.pack . show $ eval packet
     )
 
-data Instruction
-    = Input Register
-    | Add Register Value
-    | Mul Register Value
-    | Div Register Value
-    | Mod Register Value
-    | Eql Register Value
+type Instruction = (Reg, Expression)
 
-instance Show Instruction where
-    show (Input r) = "inp " ++ show r
-    show (Add r v) = "add " ++ show r ++ " " ++ show v
-    show (Mul r v) = "mul " ++ show r ++ " " ++ show v
-    show (Div r v) = "div " ++ show r ++ " " ++ show v
-    show (Mod r v) = "mod " ++ show r ++ " " ++ show v
-    show (Eql r v) = "eql " ++ show r ++ " " ++ show v
+data Expression
+    = Add Expression Expression
+    | Mul Expression Expression
+    | Div Expression Expression
+    | Mod Expression Expression
+    | Eql Expression Expression
+    | Input
+    | Register Reg
+    | Constant Int
 
-register :: Instruction -> Register
-register (Input r) = r
-register (Add r _) = r
-register (Mul r _) = r
-register (Div r _) = r
-register (Mod r _) = r
-register (Eql r _) = r
+data Reg = W | X | Y | Z deriving (Eq)
 
-value :: Instruction -> Value
-value (Input _) = ValueConstant 99
-value (Add _ v) = v
-value (Mul _ v) = v
-value (Div _ v) = v
-value (Mod _ v) = v
-value (Eql _ v) = v
+data State = State Expression Expression Expression Expression
 
-data Register = W | X | Y | Z deriving (Eq)
+-- Run a series of states starting from all registers at zero
+run :: [Instruction] -> State
+run = L.foldl' (flip interpret) initialState
+  where
+    initialState = State (Constant 0) (Constant 0) (Constant 0) (Constant 0)
 
-instance Show Register where
+interpret :: Instruction -> State -> State
+interpret (r, e) = fmap (substitute r e)
+
+-- Replace the register with the new sub-expression everywhere it appears
+substitute :: Reg -> Expression -> Expression -> Expression
+substitute r subExpr (Add p q) = Add (substitute r subExpr p) (substitute r subExpr q)
+substitute r subExpr (Mul p q) = Mul (substitute r subExpr p) (substitute r subExpr q)
+substitute r subExpr (Div p q) = Div (substitute r subExpr p) (substitute r subExpr q)
+substitute r subExpr (Mod p q) = Mod (substitute r subExpr p) (substitute r subExpr q)
+substitute r subExpr (Eql p q) = Eql (substitute r subExpr p) (substitute r subExpr q)
+substitute _ _ Input = Input
+substitute r subExpr (Register s)
+    | r == s = subExpr
+    | otherwise = Register s
+substitute _ _ (Constant x) = Constant x
+
+instance Show Expression where
+    show (Add p q) = show p ++ "+" ++ show q
+    show (Mul p q) = show p ++ "*" ++ show q
+    show (Div p q) = show p ++ "/" ++ show q
+    show (Mod p q) = show p ++ "%" ++ show q
+    show (Eql p q) = show p ++ "==" ++ show q
+    show Input = "input"
+    show (Register r) = show r
+    show (Constant x) = show x
+
+instance Show Reg where
     show W = "w"
     show X = "x"
     show Y = "y"
     show Z = "z"
 
-data Value = ValueRegister Register | ValueConstant Int
+get :: Reg -> State -> Expression
+get W (State w _ _ _) = w
+get X (State _ x _ _) = x
+get Y (State _ _ y _) = y
+get Z (State _ _ _ z) = z
 
-instance Show Value where
-    show (ValueRegister r) = show r
-    show (ValueConstant x) = show x
+put :: Reg -> Expression -> State -> State
+put W e (State w x y z) = State e x y z
+put X e (State w x y z) = State w e y z
+put Y e (State w x y z) = State w x e z
+put Z e (State w x y z) = State w x y e
+
+parse :: Text -> Instruction
+parse = parseOrStop pInstruction
 
 pInstruction :: Parser Instruction
 pInstruction = pInput <|> pArithmetic
   where
-    pRegister :: Parser Register
-    pRegister = MC.string "w" $> W <|> MC.string "x" $> X <|> MC.string "y" $> Y <|> MC.string "z" $> Z
-    pValue :: Parser Value
-    pValue = ValueRegister <$> pRegister <|> ValueConstant <$> pSignedInt
+    pReg :: Parser Reg
+    pReg = MC.string "w" $> W <|> MC.string "x" $> X <|> MC.string "y" $> Y <|> MC.string "z" $> Z
+    pValue :: Parser Expression
+    pValue = Register <$> pReg <|> Constant <$> pSignedInt
     pInput :: Parser Instruction
-    pInput = Input <$> (MC.string "inp " *> pRegister)
+    pInput = (,Input) <$> (MC.string "inp " *> pReg)
     pArithmetic :: Parser Instruction
     pArithmetic =
-        (Add <$> (MC.string "add " *> pRegister) <*> (MC.string " " *> pValue))
-            <|> (Mul <$> (MC.string "mul " *> pRegister) <*> (MC.string " " *> pValue))
-            <|> (Div <$> (MC.string "div " *> pRegister) <*> (MC.string " " *> pValue))
-            <|> (Mod <$> (MC.string "mod " *> pRegister) <*> (MC.string " " *> pValue))
-            <|> (Eql <$> (MC.string "eql " *> pRegister) <*> (MC.string " " *> pValue))
+        ((\r v -> (r, Add (Register r) v)) <$> (MC.string "add " *> pReg) <*> (MC.string " " *> pValue))
+            <|> ((\r v -> (r, Div (Register r) v)) <$> (MC.string "div " *> pReg) <*> (MC.string " " *> pValue))
+            <|> ((\r v -> (r, Mod (Register r) v)) <$> (MC.string "mod " *> pReg) <*> (MC.string " " *> pValue))
+            <|> ((\r v -> (r, Eql (Register r) v)) <$> (MC.string "eql " *> pReg) <*> (MC.string " " *> pValue))
+            <|> ((\r v -> (r, Mul (Register r) v)) <$> (MC.string "mul " *> pReg) <*> (MC.string " " *> pValue))
 
-data Expression
-    = EAdd Expression Expression
-    | EMul Expression Expression
-    | EDiv Expression Expression
-    | EMod Expression Expression
-    | EEql Expression Expression
-    | EInput
-    | ERegister Register
-    | EConstant Int
+-- for testing
+unparse :: Instruction -> String
+unparse (r, Add (Register p) q) = if r == p then "add " ++ show p ++ " " ++ show q else "Failed"
+unparse (r, Mul (Register p) q) = if r == p then "mul " ++ show p ++ " " ++ show q else "Failed"
+unparse (r, Div (Register p) q) = if r == p then "div " ++ show p ++ " " ++ show q else "Failed"
+unparse (r, Mod (Register p) q) = if r == p then "mod " ++ show p ++ " " ++ show q else "Failed"
+unparse (r, Eql (Register p) q) = if r == p then "eql " ++ show p ++ " " ++ show q else "Failed"
+unparse (r, Input) = "inp " ++ show r
+unparse _ = "Unexpected instruction"
 
-instance Show Expression where
-    show (EAdd p q) = "(" ++ "+ " ++ show p ++ " " ++ show q ++ ")"
-    show (EMul p q) = "(" ++ "* " ++ show p ++ " " ++ show q ++ ")"
-    show (EDiv p q) = "(" ++ "/ " ++ show p ++ " " ++ show q ++ ")"
-    show (EMod p q) = "(" ++ "% " ++ show p ++ " " ++ show q ++ ")"
-    show (EEql p q) = "(" ++ "== " ++ show p ++ " " ++ show q ++ ")"
-    show EInput = "input"
-    show (ERegister r) = show r
-    show (EConstant x) = show x
+-- instance Show Expression where
+--     show (EAdd p q) = "(" ++ "+ " ++ show p ++ " " ++ show q ++ ")"
+--     show (EMul p q) = "(" ++ "* " ++ show p ++ " " ++ show q ++ ")"
+--     show (EDiv p q) = "(" ++ "/ " ++ show p ++ " " ++ show q ++ ")"
+--     show (EMod p q) = "(" ++ "% " ++ show p ++ " " ++ show q ++ ")"
+--     show (EEql p q) = "(" ++ "== " ++ show p ++ " " ++ show q ++ ")"
+--     show EInput = "input"
+--     show (ERegister r) = show r
+--     show (EConstant x) = show x
+
+{-
 
 compile :: [Instruction] -> Expression
 compile = L.foldl' addInstruction (ERegister Z) . reverse
@@ -133,3 +160,5 @@ compileInstruction (Eql r v) = (r, EEql (ERegister r) (valueToExpr v))
 valueToExpr :: Value -> Expression
 valueToExpr (ValueRegister r) = ERegister r
 valueToExpr (ValueConstant x) = EConstant x
+
+-}
--- hs-test/AOC_2021_24_Test.hs
+++ hs-test/AOC_2021_24_Test.hs
@@ -2,21 +2,52 @@ module AOC_2021_24_Test where
 
 import Test.Tasty.HUnit ((@?=))
 
-import AOC_2021_24 (pInstruction)
+import AOC_2021_24 (parse, run, unparse)
 
 import Data.Text (Text)
 import Data.Text qualified as T (pack, unpack)
 
-import Utilities (parseOrStop)
+unit_2021_24_alu_parse0 :: IO ()
+unit_2021_24_alu_parse0 = unparse (parse (head program1)) @?= T.unpack (head program1)
 
 unit_2021_24_alu_parse1 :: IO ()
-unit_2021_24_alu_parse1 = show (parseOrStop pInstruction (head program1)) @?= T.unpack (head program1)
+unit_2021_24_alu_parse1 = map (T.pack . unparse . parse) program1 @?= program1
 
 unit_2021_24_alu_parse2 :: IO ()
-unit_2021_24_alu_parse2 = map (T.pack . show . parseOrStop pInstruction) program1 @?= program1
+unit_2021_24_alu_parse2 = map (T.pack . unparse . parse) program2 @?= program2
 
+unit_2021_24_alu_run0 :: IO ()
+unit_2021_24_alu_run0 = show (run (take 1 (map parse program1))) @?= "(input,x,y,z)"
+
+unit_2021_24_alu_run1 :: IO ()
+unit_2021_24_alu_run1 = show (run (take 2 (map parse program1))) @?= "(input,0,y,z)"
+
+-- First part of problem input
 program1 :: [Text]
 program1 =
+    [ "inp w"
+    , "mul x 0"
+    , "add x z"
+    , "mod x 26"
+    , "div z 1"
+    , "add x 10"
+    , "eql x w"
+    , "eql x 0"
+    , "mul y 0"
+    , "add y 25"
+    , "mul y x"
+    , "add y 1"
+    , "mul z y"
+    , "mul y 0"
+    , "add y w"
+    , "add y 2"
+    , "mul y x"
+    , "add z y"
+    ]
+
+-- Last part of problem input
+program2 :: [Text]
+program2 =
     [ "inp w"
     , "mul x 0"
     , "add x z"
