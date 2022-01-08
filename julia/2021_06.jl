706298f6f6a32dbc3b24d37aca222708ea821134
Joe Watson <jw120@users.noreply.github.com>
2022-01-08 20:23:18 +0900

    tuned 2021 6 julia

--- julia/2021_06.jl
+++ julia/2021_06.jl
@@ -3,13 +3,9 @@
 using Test
 
 function fish(initial_timers::AbstractVector{Int}, number_of_steps::Int)::Int
-    f = Dict()
-    for i in range(-8, number_of_steps)
-        if i <= 0
-            f[i] = 1
-        else
-            f[i] = f[i-1-8] + f[i-1-6]
-        end
+    f = Dict((i, 1) for i in range(-8, 0))
+    for i in range(1, number_of_steps)
+        f[i] = f[i-1-8] + f[i-1-6]
     end
     sum(f[number_of_steps-x] for x in initial_timers)
 end
@@ -19,25 +15,3 @@ end
 initial = [parse(Int, s) for s in split(read("../aoc-data/input/2021_06.txt", String), ",")]
 println(fish(initial, 80))
 println(fish(initial, 256))
-
-
-# def fish(initial_timers: list[int], number_of_steps: int) -> int:
-#     """Return number of fish after given number of steps from initial position.
-
-#     >>> [fish(test_data, i) for i in [1, 2, 18, 256]]
-#     [5, 6, 26, 26984457539]
-#     """
-#     f: Dict[int, int] = {}  # number of fish after n steps from one timer-0 fish
-#     for i in range(-8, number_of_steps):
-#         if i <= 0:
-#             f[i] = 1
-#         else:
-#             f[i] = f[i - 1 - 8] + f[i - 1 - 6]
-#     return sum(f[number_of_steps - x] for x in initial_timers)
-
-
-# if __name__ == "__main__":
-#     testmod()
-#     initial: list[int] = [int(x) for x in stdin.read().split(",")]
-#     print(fish(initial, 80))
-#     print(fish(initial, 256))
