5c308c632c45267c377e1ebcc8410f481333899b
Joe Watson <jw120@users.noreply.github.com>
2022-12-02 19:27:12 +0800

    2022 1-2 done

---
+++ python/aoc_2022_02.py
@@ -0,0 +1,79 @@
+"""Advent of Code 2022 - Day 2."""
+
+from doctest import testmod
+from enum import Enum
+from sys import stdin
+from typing import NoReturn, Tuple
+
+
+def assert_never(value: NoReturn) -> NoReturn:
+    assert False, f"This code should never be reached, got: {value}"
+
+
+class Move(Enum):
+    rock = "rock"
+    paper = "paper"
+    scissors = "scissors"
+
+
+beats = {Move.rock: Move.scissors, Move.paper: Move.rock, Move.scissors: Move.paper}
+beaten = {q: p for p, q in beats.items()}
+opponent_moves = {"A": Move.rock, "B": Move.paper, "C": Move.scissors}
+player_moves = {"X": Move.rock, "Y": Move.paper, "Z": Move.scissors}
+
+
+class Result(Enum):
+    lose = "lose"
+    draw = "draw"
+    win = "win"
+
+
+results = {"X": Result.lose, "Y": Result.draw, "Z": Result.win}
+
+
+def read_moves(s: str) -> Tuple[Move, Move]:
+    """Read a line as a pair of moves."""
+    [opponent, player] = s.split()
+    return (opponent_moves[opponent], player_moves[player])
+
+
+def read_result(s: str) -> Tuple[Move, Result]:
+    """Read a line as a move and a result."""
+    [opponent, result] = s.split()
+    return (opponent_moves[opponent], results[result])
+
+
+def score_moves(moves: Tuple[Move, Move]) -> int:
+    """Score a game given the two moves."""
+    opponent, player = moves
+    shape_score = 1 if player == Move.rock else 2 if player == Move.paper else 3
+    if beats[opponent] == player:
+        win_score = 0
+    elif beats[player] == opponent:
+        win_score = 6
+    elif player == opponent:
+        win_score = 3
+    else:
+        raise ValueError("Unknown outcome!")
+    return win_score + shape_score
+
+
+def score_result(game: Tuple[Move, Result]) -> int:
+    """Score a game given the opponent moves and desired result."""
+    opponent, result = game
+    if result == Result.lose:
+        player = beats[opponent]
+    elif result == Result.win:
+        player = beaten[opponent]
+    elif result == Result.draw:
+        player = opponent
+    else:
+        assert_never(result)
+    return score_moves((opponent, player))
+
+
+if __name__ == "__main__":
+    testmod()
+    lines = list(stdin)
+    print(sum(score_moves(read_moves(line)) for line in lines))
+    print(sum(score_result(read_result(line)) for line in lines))
---
+++ python/aoc_2022_02.txt
@@ -0,0 +1,37 @@
+"""Advent of Code 2022 - Day 1."""
+
+from doctest import testmod
+from sys import stdin
+from typing import Optional
+
+
+def sum_groups(xs: list[Optional[int]]) -> list[int]:
+    """Return sums of groups of lines separated by None.
+
+    >>> sum_groups([1, 2, 3, None, 4, None, 5, 7])
+    [6, 4, 12]
+    """
+    ys: list[int] = []
+    group_sum: int = 0
+    group_count: int = 0
+    for x in xs:
+        if x is None:
+            ys.append(group_sum)
+            group_sum = 0
+            group_count = 0
+        else:
+            group_sum += x
+            group_count += 1
+    if group_count:
+        ys.append(group_sum)
+    return ys
+
+
+if __name__ == "__main__":
+    testmod()
+    calories: list[Optional[int]] = [
+        (int(line) if line.strip() else None) for line in stdin
+    ]
+    elves = sorted(sum_groups(calories), reverse=True)
+    print(elves[0])
+    print(sum(elves[:3]))
