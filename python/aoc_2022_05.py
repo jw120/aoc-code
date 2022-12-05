f2133dd1232a26e1940b3df9f96ff2ddbf18c17b
Joe Watson <jw120@users.noreply.github.com>
2022-12-06 00:16:19 +0800

    2022 5 half done

--- python/aoc_2022_05.py
+++ python/aoc_2022_05.py
@@ -31,6 +31,8 @@ test_crates = """    [D]____
 
 move 1 from 2 to 1
 move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
 """
 
 
@@ -39,17 +41,13 @@ class Crates:
         """Initialize the crates from the given input.
 
         >>> Crates(test_crates.replace('_', ' '))
-        {1: ZN, 2: MCD, 3: P, [(1, 2, 1), (3, 1, 3)]}
+        {1: ZN, 2: MCD, 3: P, [(1, 2, 1), (3, 1, 3), (2, 2, 1), (1, 1, 2)]}
         """
         crate_block, move_block = s.split("\n\n")
         crate_lines = crate_block.split("\n")[:-1]  # Drop row with stack numbers
-        # print("crate_lines\n", crate_lines)
         move_lines = move_block.split("\n")
-        # print("move_lines\n", move_lines)
         highest_stack = len(crate_lines)
-        # print("highest stack", highest_stack)
         self.number_of_stacks: int = (len(crate_lines[0]) + 1) // 4
-        # print("num stacks", self.number_of_stacks)
         self.stacks: dict[int, list[str]] = {
             i: [] for i in range(1, self.number_of_stacks + 1)
         }
@@ -60,7 +58,6 @@ class Crates:
         for row in range(0, highest_stack):
             for stack in range(1, self.number_of_stacks + 1):
                 crate = crate_lines[-row - 1][stack * 4 - 3]
-                # print("row", row, "stack", stack, ":", crate)
                 if crate.isalpha():
                     self.stacks[stack].append(crate)
         self.moves: list[Move] = [read_move(line) for line in move_lines if line]
@@ -73,8 +70,29 @@ class Crates:
         moves = str([(m.number, m.source, m.destination) for m in self.moves])
         return "{" + crates + ", " + moves + "}"
 
+    def apply_moves(self) -> None:
+        """Apply the moves.
+
+        >>> c = Crates(test_crates.replace('_', ' '))
+        >>> c.apply_moves()
+        >>> c
+        {1: C, 2: M, 3: PDNZ, [(1, 2, 1), (3, 1, 3), (2, 2, 1), (1, 1, 2)]}
+        """
+        # print(self.stacks)
+        for move in self.moves:
+            moving = self.stacks[move.source][-move.number :]
+            # print("moving", moving)
+            self.stacks[move.source][-move.number :] = []
+            self.stacks[move.destination] += list(reversed(moving))
+            # print(self.stacks)
+
+    def tops(self) -> str:
+        """Top crate on each stack."""
+        return "".join(self.stacks[i][-1] for i in range(1, self.number_of_stacks + 1))
+
 
 if __name__ == "__main__":
     testmod()
     crates = Crates(stdin.read())
-    print(repr(crates))
+    crates.apply_moves()
+    print(crates.tops())
