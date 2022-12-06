07b3790fd451287ca6f67d8d02ac0bf3ce0bf3e2
Joe Watson <jw120@users.noreply.github.com>
2022-12-06 20:50:10 +0800

    2022 5-6 done

--- python/aoc_2022_05.py
+++ python/aoc_2022_05.py
@@ -1,4 +1,4 @@
-"""Advent of Code 2022 - Day 4."""
+"""Advent of Code 2022 - Day 5."""
 
 from dataclasses import dataclass
 from doctest import testmod
@@ -10,6 +10,9 @@ class Move:
     number: int
     source: int
     destination: int
+
+    def show(self) -> None:
+        print(f"{(self.number, self.source, self.destination)}")
 
 
 def read_move(s: str) -> Move:
@@ -24,6 +27,66 @@ def read_move(s: str) -> Move:
     return Move(number=int(number), source=int(source), destination=int(destination))
 
 
+Stacks = dict[int, list[str]]
+
+
+def read_problem(s: str) -> tuple[Stacks, list[Move]]:
+    """Read problem and return initial stacks and moves.
+
+    >>> stacks, moves = read_problem(test_crates.replace('_', ' '))
+    >>> show_stacks(stacks)
+    {1: ZN, 2: MCD, 3: P}
+    >>> moves[2]
+    Move(number=2, source=2, destination=1)
+    """
+    stack_block, move_block = s.split("\n\n")
+    stack_lines = stack_block.split("\n")[:-1]  # Drop row with stack numbers
+    move_lines = move_block.split("\n")
+    highest_stack = len(stack_lines)
+    number_of_stacks: int = (len(stack_lines[0]) + 1) // 4
+    stacks: Stacks = {i: [] for i in range(1, number_of_stacks + 1)}
+    for line in stack_lines:
+        assert len(line) == number_of_stacks * 4 - 1, f"Bad '{line}' {stack_lines}"
+    for row in range(0, highest_stack):
+        for stack in range(1, number_of_stacks + 1):
+            crate = stack_lines[-row - 1][stack * 4 - 3]
+            if crate.isalpha():
+                stacks[stack].append(crate)
+    moves: list[Move] = [read_move(line) for line in move_lines if line]
+    return (stacks, moves)
+
+
+def apply_moves(stacks: Stacks, moves: list[Move], reverse: bool) -> Stacks:
+    """Apply the moves (without mutating).
+
+    >>> stacks, moves = read_problem(test_crates.replace('_', ' '))
+    >>> show_stacks(apply_moves(stacks, moves, reverse=True))
+    {1: C, 2: M, 3: PDNZ}
+    >>> show_stacks(apply_moves(stacks, moves, reverse=False))
+    {1: M, 2: C, 3: PZND}
+    """
+    new_stacks: Stacks = {i: x.copy() for i, x in stacks.items()}
+    for move in moves:
+        moving = new_stacks[move.source][-move.number :]
+        if reverse:
+            moving = list(reversed(moving))
+        # print("moving", moving)
+        new_stacks[move.source][-move.number :] = []
+        new_stacks[move.destination] += moving
+    return new_stacks
+
+
+def tops(stacks: Stacks) -> str:
+    """Top crate on each stack."""
+    return "".join(stacks[i][-1] for i in range(1, len(stacks) + 1))
+
+
+def show_stacks(stacks: Stacks) -> None:
+    crates = ", ".join(f"{i}: {''.join(stacks[i])}" for i in range(1, len(stacks) + 1))
+    print("{" + crates + "}")
+
+
+# Test data for doctest (underscores used to preserve trailing spaces)
 test_crates = """    [D]____
 [N] [C]____
 [Z] [M] [P]
@@ -36,63 +99,8 @@ move 1 from 1 to 2
 """
 
 
-class Crates:
-    def __init__(self, s: str) -> None:
-        """Initialize the crates from the given input.
-
-        >>> Crates(test_crates.replace('_', ' '))
-        {1: ZN, 2: MCD, 3: P, [(1, 2, 1), (3, 1, 3), (2, 2, 1), (1, 1, 2)]}
-        """
-        crate_block, move_block = s.split("\n\n")
-        crate_lines = crate_block.split("\n")[:-1]  # Drop row with stack numbers
-        move_lines = move_block.split("\n")
-        highest_stack = len(crate_lines)
-        self.number_of_stacks: int = (len(crate_lines[0]) + 1) // 4
-        self.stacks: dict[int, list[str]] = {
-            i: [] for i in range(1, self.number_of_stacks + 1)
-        }
-        for line in crate_lines:
-            assert (
-                len(line) == self.number_of_stacks * 4 - 1
-            ), f"Bad '{line}' {crate_lines}"
-        for row in range(0, highest_stack):
-            for stack in range(1, self.number_of_stacks + 1):
-                crate = crate_lines[-row - 1][stack * 4 - 3]
-                if crate.isalpha():
-                    self.stacks[stack].append(crate)
-        self.moves: list[Move] = [read_move(line) for line in move_lines if line]
-
-    def __repr__(self) -> str:
-        crates = ", ".join(
-            f"{i}: {''.join(self.stacks[i])}"
-            for i in range(1, self.number_of_stacks + 1)
-        )
-        moves = str([(m.number, m.source, m.destination) for m in self.moves])
-        return "{" + crates + ", " + moves + "}"
-
-    def apply_moves(self) -> None:
-        """Apply the moves.
-
-        >>> c = Crates(test_crates.replace('_', ' '))
-        >>> c.apply_moves()
-        >>> c
-        {1: C, 2: M, 3: PDNZ, [(1, 2, 1), (3, 1, 3), (2, 2, 1), (1, 1, 2)]}
-        """
-        # print(self.stacks)
-        for move in self.moves:
-            moving = self.stacks[move.source][-move.number :]
-            # print("moving", moving)
-            self.stacks[move.source][-move.number :] = []
-            self.stacks[move.destination] += list(reversed(moving))
-            # print(self.stacks)
-
-    def tops(self) -> str:
-        """Top crate on each stack."""
-        return "".join(self.stacks[i][-1] for i in range(1, self.number_of_stacks + 1))
-
-
 if __name__ == "__main__":
     testmod()
-    crates = Crates(stdin.read())
-    crates.apply_moves()
-    print(crates.tops())
+    stacks, moves = read_problem(stdin.read())
+    print(tops(apply_moves(stacks, moves, reverse=True)))
+    print(tops(apply_moves(stacks, moves, reverse=False)))
---
+++ python/aoc_2022_06.py
@@ -0,0 +1,41 @@
+"""Advent of Code 2022 - Day 6."""
+
+from doctest import testmod
+from sys import stdin
+
+
+def packet_start(s: str) -> int:
+  """Return number of characters processed before first start-of-packet marker.
+
+    Marker is four consecutive characters that are unique.
+
+    >>> packet_start("bvwbjplbgvbhsrlpgdmjqwftvncz")
+    5
+    """
+  for n, cs in enumerate(zip(s, s[1:], s[2:], s[3:]), start=4):
+    if len(set(cs)) == 4:
+      return n
+  raise ValueError("Failed to find start-of-packet marker")
+
+
+def message_start(s: str) -> int:
+  """Return number of characters processed before first start-of-message marker.
+
+    Marker is fourteen consecutive characters that are unique.
+
+    >>> message_start("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+    19
+    """
+  tuples = zip(s, s[1:], s[2:], s[3:], s[4:], s[5:], s[6:], s[7:], s[8:],
+               s[9:], s[10:], s[11:], s[12:], s[13:])
+  for n, cs in enumerate(tuples, start=14):
+    if len(set(cs)) == 14:
+      return n
+  raise ValueError("Failed to find start-of-message marker")
+
+
+if __name__ == "__main__":
+  testmod()
+  message = stdin.read()
+  print(packet_start(message))
+  print(message_start(message))
