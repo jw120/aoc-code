8042a69de3e68015761750ca6b7a386997cdb6c0
Joe Watson <jw120@users.noreply.github.com>
2022-12-10 15:09:53 +0800

    2022 7 8 done

---
+++ python/aoc_2022_07.py
@@ -0,0 +1,103 @@
+"""Advent of Code 2022 - Day 7."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+from sys import stdin
+from typing import Union
+
+
+@dataclass
+class File:
+    """A file in the directory tree."""
+
+    size: int
+
+
+@dataclass
+class NonRootDir:
+    """A directory (other than '/') in the directory tree."""
+
+    contents: dict[str, Content]
+    parent: Dir
+
+
+@dataclass
+class RootDir:
+    """The root directory in the directory tree."""
+
+    contents: dict[str, Content]
+
+
+Dir = Union[NonRootDir, RootDir]
+Content = Union[NonRootDir, File]
+
+
+def build(terminal_output: list[str]) -> RootDir:
+    """Build root directory storage from terminal output."""
+    root_dir: RootDir = RootDir(contents={})
+    current_dir: Dir = root_dir
+    listing_mode: bool = True
+
+    for line in terminal_output:
+
+        if listing_mode:
+            match line.split():
+                case ["dir", name]:
+                    current_dir.contents[name] = NonRootDir(
+                        contents={}, parent=current_dir
+                    )
+                    continue
+                case [size, name]:
+                    current_dir.contents[name] = File(size=int(size))
+                    continue
+                case ["$", *_]:
+                    listing_mode = False
+                case _:
+                    raise ValueError(f"Bad listing line: {line}")
+
+        match line.split():
+            case ["$", "cd", "/"]:
+                current_dir = root_dir
+            case ["$", "cd", ".."]:
+                if isinstance(current_dir, NonRootDir):
+                    current_dir = current_dir.parent
+            case ["$", "cd", name]:
+                node = current_dir.contents[name]
+                if not isinstance(node, NonRootDir):
+                    raise ValueError(f"Can't cd to non-directory {line}")
+                current_dir = node
+            case ["$", "ls"]:
+                listing_mode = True
+            case _:
+                raise ValueError(f"Bad command: {line}")
+
+    return root_dir
+
+
+def walk_sizes(d: Dir) -> list[int]:
+    """Return sizes of all sub-directories and this directory.
+
+    This directory is the last element of the list.
+    """
+    total_size = 0
+    dir_sizes = []
+    for node in d.contents.values():
+        match node:
+            case File(size=size):
+                total_size += size
+            case NonRootDir() as sub_dir:
+                sub_dir_sizes = walk_sizes(sub_dir)
+                total_size += sub_dir_sizes[-1]
+                dir_sizes += sub_dir_sizes
+    dir_sizes.append(total_size)
+    return dir_sizes
+
+
+if __name__ == "__main__":
+    terminal_output = [line.strip() for line in stdin]
+    dir_sizes = walk_sizes(build(terminal_output))
+    print(sum(size for size in dir_sizes if size <= 100000))
+    current_space = 70000000 - dir_sizes[-1]
+    space_needed = 30000000 - current_space
+    print(min(size for size in dir_sizes if size >= space_needed))
---
+++ python/aoc_2022_08.py
@@ -0,0 +1,113 @@
+"""Advent of Code 2022 - Day 8."""
+
+# from __future__ import annotations
+
+from doctest import testmod
+from sys import stdin
+
+test_input: list[list[int]] = [
+    [3, 0, 3, 7, 3],
+    [2, 5, 5, 1, 2],
+    [6, 5, 3, 3, 2],
+    [3, 3, 5, 4, 9],
+    [3, 5, 3, 9, 0],
+]
+
+
+def visible(m: list[list[int]]) -> list[list[bool]]:
+    """Return matrix showing which tree are visible from outside the grid.
+
+    >>> test_visible = visible(test_input)
+    >>> ["".join("*" if b else "." for b in row) for row in test_visible]
+    ['*****', '***.*', '**.**', '*.*.*', '*****']
+    >>> sum(sum(row) for row in test_visible)
+    21
+    """
+
+    rows = len(m)
+    cols = len(m[0])
+
+    # Start with everything not visible
+    v = [[False for _ in row] for row in m]
+
+    # Scan left and right
+    for i in range(rows):
+        h_max = -1
+        for j in range(cols):
+            if m[i][j] > h_max:
+                v[i][j] = True
+                h_max = m[i][j]
+        h_max = -1
+        for j in reversed(range(cols)):
+            if m[i][j] > h_max:
+                v[i][j] = True
+                h_max = m[i][j]
+
+    # Scan up and down
+    for j in range(cols):
+        h_max = -1
+        for i in range(rows):
+            if m[i][j] > h_max:
+                v[i][j] = True
+                h_max = m[i][j]
+        h_max = -1
+        for i in reversed(range(rows)):
+            if m[i][j] > h_max:
+                v[i][j] = True
+                h_max = m[i][j]
+
+    return v
+
+
+def most_scenic(m: list[list[int]]) -> int:
+    """Return highest scenic score.
+
+    >>> most_scenic(test_input)
+    8
+    """
+    return max(scenic(m, i, j) for j in range(len(m[0])) for i in range(len(m)))
+
+
+def scenic(m: list[list[int]], x: int, y: int) -> int:
+    """Return scenic score of a tree.
+
+    >>> scenic(test_input, 1 , 2)
+    4
+    >>> scenic(test_input, 3 , 2)
+    8
+    """
+
+    # rows = len(m)
+    cols = len(m[0])
+
+    h = m[x][y]
+
+    right = 0
+    for q in range(y + 1, cols):
+        right += 1
+        if m[x][q] >= h:
+            break
+    left = 0
+    for q in range(y - 1, -1, -1):
+        left += 1
+        if m[x][q] >= h:
+            break
+    up = 0
+    for p in range(x - 1, -1, -1):
+        up += 1
+        if m[p][y] >= h:
+            break
+    down = 0
+    for p in range(x + 1, cols):
+        down += 1
+        if m[p][y] >= h:
+            break
+
+    return left * right * up * down
+
+
+if __name__ == "__main__":
+    testmod()
+    m = [[int(c) for c in line.strip()] for line in stdin]
+    print(sum(sum(b for b in row) for row in visible(m)))
+    print(most_scenic(m))
