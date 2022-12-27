3fdcd2a86e9ef9179444a5b575b1b2db50755190
Joe Watson <jw120@users.noreply.github.com>
2022-12-28 08:26:46 +0900

    Work on 2025 24

--- python/aoc_2022_24.py
+++ python/aoc_2022_24.py
@@ -1,12 +1,12 @@
 """Advent of Code 2022 - Day 24."""
 
 
-from dataclasses import dataclass, field
-
-from doctest import testmod
 import fileinput
-from heapq import heappush, heappop
+from dataclasses import dataclass, field
+from doctest import testmod
+from heapq import heappop, heappush
 from itertools import chain
+from math import lcm
 from typing import Final, Iterable
 
 # x runs left-ro-right, y runs top-to-bottom
@@ -21,24 +21,28 @@ class Blizzard:
     step: Coord
 
 
-@dataclass(frozen=True, order=True)
+@dataclass(frozen=True)
 class State:
-    """State for walk of available paths.
+    """State for walk of available paths."""
 
-    Ordering (for A*) is on priority only
-    """
+    location: Coord
+    time: int
 
-    location: Coord = field(compare=False)
-    time: int = field(compare=False)
+
+@dataclass(frozen=True, order=True)
+class PrioritizedState:
+    """State with priority for A*."""
+
+    state: State = field(compare=False, hash=True)
     priority: int
 
 
 class Basin:
     """Main class for day 24."""
 
-    def __init__(self, input_lines: Iterable[str]) -> None:
+    def __init__(self, input_lines: Iterable[str], debug: bool = False) -> None:
         self.initial_blizzards: list[Blizzard] = []
-        self.cached_blizzards: dict[int, set[Coord]] = {}
+        self.cached: dict[State, bool] = {}
         for row, line in enumerate(input_lines, start=-1):
             line = line.strip()
             if row == -1:
@@ -73,78 +77,101 @@ class Basin:
                     )
         self.start = Coord(0, -1)
         self.goal = Coord(self.width - 1, self.height)
+        self.repeat = lcm(self.width, self.height)
+        self.debug = debug
+        if self.debug:
+            print(
+                f"{self.width}x{self.height}: repeat: {self.repeat} max_cache {self.repeat*self.width*self.height}"
+            )
 
-    def will_be_empty(self, c: Coord, t: int) -> int:
-        """Test if coordinate will be empty after given number of minutes."""
-        if c.y <= -1 and c != self.start:  # Top wall
-            return False
-        if c.y >= self.height and c != self.goal:  # Bottom wall
-            return False
-        if c.x in (-1, self.width):  # Side walls
-            return False
-        if t in self.cached_blizzards:
-            b = self.cached_blizzards[t]
-        else:
-            print(t)
-            b = set()
+    def will_be_empty(self, state: State) -> bool:
+        """Test if state will be empty."""
+        if state.location.y <= -1 and state.location != self.start:
+            return False  # Top wall
+        if state.location.y >= self.height and state.location != self.goal:
+            return False  # Bottom wall
+        if state.location.x in (-1, self.width):
+            return False  # Side walls
+        wrapped_state = State(location=state.location, time=state.time % self.repeat)
+        try:
+            return self.cached[wrapped_state]
+        except KeyError:
+            to_cache = True
             for blizzard in self.initial_blizzards:
-                x = (blizzard.start.x + t * blizzard.step.x) % self.width
-                y = (blizzard.start.y + t * blizzard.step.y) % self.height
-                b.add(Coord(x, y))
-            self.cached_blizzards[t] = b
-        return c not in b
+                if (
+                    blizzard.start.x != state.location.x
+                    and blizzard.start.y != state.location.y
+                ):
+                    continue  # Blizzards only move up/down/left/right
+                x = (blizzard.start.x + state.time * blizzard.step.x) % self.width
+                y = (blizzard.start.y + state.time * blizzard.step.y) % self.height
+                if state.location == Coord(x, y):
+                    to_cache = False
+                    break
+            self.cached[wrapped_state] = to_cache
+            return to_cache
 
-    def priority(self, c: Coord, t: int) -> int:
-        """Return the priority of the given coordinate.
+    def add_priority(self, state: State) -> PrioritizedState:
+        """Add priority to the state.
 
         Used by the A*-algorithm. Defined as distances from origin
         plus distance to goal (manhattan distances)."""
-        return t + manhattan(c, self.goal)
-
-    def find_path(self) -> int:
-        """Return length of shortest path to goal.
-
-        >>> Basin(TEST_DATA2).find_path()
-        18
-        """
-        initial_state = State(
-            location=self.start,
-            time=0,
-            priority=self.priority(self.start, 0),
+        return PrioritizedState(
+            state=state, priority=state.time + manhattan(state.location, self.goal)
         )
-        heap: list[State] = []
-        heappush(heap, initial_state)
-        visited: set[tuple[Coord, int]] = set()
+
+    def path(self, start_location: Coord, goal_location: Coord, start_time: int) -> int:
+        """Return length of shortest path with given start time."""
+        initial_state: State = State(
+            location=start_location,
+            time=start_time,
+        )
+        heap: list[PrioritizedState] = []
+        heappush(heap, self.add_priority(initial_state))
+        visited: set[State] = set()
+        t_max: int = -1
 
         while True:
             if not heap:
                 raise ValueError("No path found")
-            state = heappop(heap)
-            visited.add((state.location, state.time))
-            # print(
-            #     f"p={state.priority} t={state.time}: ({state.location.x}, {state.location.y}), {len(visited)} heap: {len(heap)}",
-            #     [(c.x, c.y, t) for c, t in visited],
-            # )
-            # print(
-            #     f"({state.location.x}, {state.location.y}), {len(visited)} heap: {len(heap)}"
-            # )
-            if state.location == self.goal:
+            state: State = heappop(heap).state
+            visited.add(state)
+            if state.time > t_max:
+                t_max = state.time
+                if self.debug and t_max % 10 == 0:
+                    print(
+                        f"t_max={t_max}, cache size={len(self.cached)}",
+                        f"{round(100 * len(self.cached)/(self.width * self.height * self.repeat), 1)}%",
+                    )
+                    if t_max == 100:
+                        return -1
+            if state.location == goal_location:
                 return state.time
             for adj in chain(state.location.adjacents(), [state.location]):
                 # print(f"Considering move from {state.location} to {adj}")
-                if (adj, state.time + 1) not in visited and self.will_be_empty(
-                    adj, state.time + 1
-                ):
-                    heappush(
-                        heap,
-                        State(
-                            location=adj,
-                            time=state.time + 1,
-                            # visited=state.visited | frozenset([state.location]),
-                            priority=self.priority(adj, state.time + 1),
-                        ),
-                    )
+                trial_state = State(location=adj, time=state.time + 1)
+                if trial_state not in visited and self.will_be_empty(trial_state):
+                    heappush(heap, self.add_priority(trial_state))
                     # print("added")
+
+    def forward_path(self, start_time: int) -> int:
+        """Return length of shortest path from start to goal with given start time.
+
+        >>> basin = Basin(TEST_DATA2)
+        >>> basin.forward_path(0)
+        18
+        >>> basin.forward_path(41)
+        54
+        """
+        return self.path(self.start, self.goal, start_time)
+
+    def backward_path(self, start_time: int) -> int:
+        """Return length of path from goal to start with given start time.
+
+        >>> Basin(TEST_DATA2).backward_path(18)
+        41
+        """
+        return self.path(self.goal, self.start, start_time)
 
     def show(self, t: int) -> None:
         """Print debugging information for time t."""
@@ -152,7 +179,8 @@ class Basin:
         for row in range(self.height):
             print("#", end="")
             for col in range(self.width):
-                print("." if self.will_be_empty(Coord(col, row), t) else "*", end="")
+                state = State(location=Coord(col, row), time=t)
+                print("." if self.will_be_empty(state) else "*", end="")
             print("#")
         print("#" * self.width + ".#")
 
@@ -180,6 +208,10 @@ TEST_DATA2: Final[
 
 if __name__ == "__main__":
     testmod()
-    basin = Basin(fileinput.input())
+    basin = Basin(fileinput.input(), debug=True)
     # basin.show(0)
-    print(basin.find_path())
+    TIME1 = basin.forward_path(0)
+    # print(TIME1)
+    # TIME2 = basin.backward_path(TIME1)
+    # TIME3 = basin.forward_path(TIME2)
+    # print(TIME3)
