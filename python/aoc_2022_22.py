1078e4a200e041d65aea64a50361b6975f0ce798
Joe Watson <jw120@users.noreply.github.com>
2023-01-11 21:19:29 +0900

    more on 2022 22

--- python/aoc_2022_22.py
+++ python/aoc_2022_22.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-# from doctest import testmod
+from doctest import testmod
 from enum import Enum
 from re import findall
 from sys import stdin
@@ -37,21 +37,21 @@ class Direction(Enum):
 FaceOffset: TypeAlias = tuple[int, int]
 
 # We define cube topology by showing for each face, if we leave the face in the given direction
-# then we appear on the given face, its edge and heading in the given direction
+# then we appear on the given face and edge with coordinate sense preserved or not
 Topology: TypeAlias = dict[
-    FaceOffset, dict[Direction, tuple[FaceOffset, Direction, Direction]]
+    FaceOffset, dict[Direction, tuple[FaceOffset, Direction, bool]]
 ]
 
 
 # For the simple wrapping topology for the quesion example, we use a helper function
 def wrap(
     u: FaceOffset, r: FaceOffset, d: FaceOffset, l: FaceOffset
-) -> dict[Direction, tuple[FaceOffset, Direction, Direction]]:
+) -> dict[Direction, tuple[FaceOffset, Direction, bool]]:
     return {
-        Direction.UP: (u, Direction.DOWN, Direction.UP),
-        Direction.RIGHT: (r, Direction.LEFT, Direction.RIGHT),
-        Direction.DOWN: (d, Direction.UP, Direction.DOWN),
-        Direction.LEFT: (l, Direction.RIGHT, Direction.LEFT),
+        Direction.UP: (u, Direction.DOWN, True),
+        Direction.RIGHT: (r, Direction.LEFT, True),
+        Direction.DOWN: (d, Direction.UP, True),
+        Direction.LEFT: (l, Direction.RIGHT, True),
     }
 
 
@@ -73,23 +73,63 @@ MAIN_WRAP_TOPOLOGY: Final[Topology] = {
     (0, 3): wrap((0, 2), (0, 3), (0, 2), (0, 3)),
 }
 
+TEST_FOLD_TOPOLOGY: Final[Topology] = {
+     (2, 0): {
+         Direction.UP: ((0, 1), Direction.UP, False),
+         Direction.RIGHT: ((3, 2), Direction.RIGHT, False),
+         Direction.DOWN: ((2, 1), Direction.UP, True),
+         Direction.LEFT: ((1, 1), Direction.UP, True)
+         },
+    (0, 1): {
+         Direction.UP: ((2, 0), Direction.UP, False),
+         Direction.RIGHT: ((1, 1), Direction.LEFT, True),
+         Direction.DOWN: ((2,2 ), Direction.DOWN, False),
+         Direction.LEFT: ((3,2 ), Direction.DOWN, False)
+         },
+    (1, 1): {
+         Direction.UP: ((2, 0), Direction.LEFT, True),
+         Direction.RIGHT: ((2,1 ), Direction.LEFT, True),
+         Direction.DOWN: ((2, 2), Direction.LEFT, False),
+         Direction.LEFT: ((0, 1), Direction.RIGHT, True)
+        },
+    (2, 1): {
+         Direction.UP: ((2, 0), Direction.DOWN, True),
+         Direction.RIGHT: ((3, 2), Direction.UP, False),
+         Direction.DOWN: ((2, 2), Direction.UP, True),
+         Direction.LEFT: ((1, 1), Direction.RIGHT, True)
+        },
+    (2, 2): {
+         Direction.UP: ((2, 1), Direction.DOWN, True),
+         Direction.RIGHT: ((3, 2), Direction.LEFT, True),
+         Direction.DOWN: ((0, 1), Direction.DOWN, False),
+         Direction.LEFT: ((1, 1), Direction.DOWN, False)
+        },
+    (3, 2): {
+         Direction.UP: ((2, 1), Direction.RIGHT, False),
+         Direction.RIGHT: ((2, 0), Direction.RIGHT, False),
+         Direction.DOWN: ((0, 1), Direction.LEFT, False),
+         Direction.LEFT: ((2, 2), Direction.RIGHT, True)
+    }}
 
-def assert_wrap(t: Topology) -> None:
+
+def assert_wrap(t: Topology, wrap: bool) -> None:
     """Check that a wrap-around topology connects properly."""
     for f_from, m in t.items():
         for d in [Direction.UP, Direction.RIGHT, Direction.DOWN, Direction.LEFT]:
-            f_to, e_to, d_to = m[d]
-            assert e_to == d.opposite()  # Arrive on edge opposite to exit direction
-            assert d_to == d  # Arrive going in same direction as leaving
+            f_to, e_to, preserve = m[d]
+            if wrap:
+                assert e_to == d.opposite()  # Arrive on edge opposite to exit direction
+                assert preserve
             # Check reverse connection
-            f_back, e_back, d_back = t[f_to][d.opposite()]
+            f_back, e_back, preserve_back = t[f_to][e_to]
             assert f_back == f_from  # Arrive back on starting face
             assert e_back == d  # Arrive back on edge we left from
-            assert d_back == d.opposite()  # Arrive back in revese direction
+            assert preserve_back == preserve
 
 
-assert_wrap(TEST_WRAP_TOPOLOGY)
-assert_wrap(MAIN_WRAP_TOPOLOGY)
+assert_wrap(TEST_WRAP_TOPOLOGY, wrap=True)
+assert_wrap(MAIN_WRAP_TOPOLOGY, wrap=True)
+assert_wrap(TEST_FOLD_TOPOLOGY, wrap=False)
 
 
 class MonkeyMap:
@@ -97,7 +137,7 @@ class MonkeyMap:
 
     board: dict[Coord, bool]  # True for walls
     extent: Extent
-    x_block_size: int
+    block_size: int
     y_block_size: int
     path: list[str]
 
@@ -128,54 +168,61 @@ class MonkeyMap:
                     case _:
                         raise ValueError(f"Bad character in board input '{c}'")
 
-        self.x_block_size = self.extent.x // self.face_extent.x
-        self.y_block_size = self.extent.y // self.face_extent.y
+        self.block_size = self.extent.x // self.face_extent.x
+        assert  self.extent.y // self.face_extent.y == self.block_size
         assert (
-            self.x_block_size
-            * self.y_block_size
+            self.block_size
+            * self.block_size
             * self.face_extent.x
             * self.face_extent.y
             == self.extent.x * self.extent.y
         ), f"Bad size: {self.extent} {self.face_extent}"
         self.path: list[str] = findall(r"\d+|L|R", path_str)
+#        print(self.extent, self.face_extent, self.block_size)
 
-    def to_coord(self, c: Coord, face: FaceOffset, edge: Direction) -> Coord:
+    def to_coord(self, c: Coord, d: Direction, face: FaceOffset, edge: Direction, preserve: bool) -> Coord:
         """Return the coordinate on the given face and edge."""
-        c_relative = Coord(c.x % self.x_block_size, c.y % self.y_block_size)
+        c_relative = Coord(c.x % self.block_size, c.y % self.block_size)
+        if d in (Direction.UP, Direction.DOWN):
+            offset = c_relative.x
+        else:
+            offset = c_relative.y
+        if not preserve:
+            offset = self.block_size - 1 - offset
         match edge:
             case Direction.UP:
-                c_relative = Coord(c_relative.x, 0)
+                c_relative = Coord(offset, 0)
             case Direction.RIGHT:
-                c_relative = Coord(self.x_block_size - 1, c_relative.y)
+                c_relative = Coord(self.block_size - 1, offset)
             case Direction.DOWN:
-                c_relative = Coord(c_relative.x, self.y_block_size - 1)
+                c_relative = Coord(offset, self.block_size - 1)
             case Direction.LEFT:
-                c_relative = Coord(0, c_relative.y)
+                c_relative = Coord(0, offset)
         face_x, face_y = face
         return (
-            Coord(face_x * self.x_block_size, face_y * self.y_block_size) + c_relative
+            Coord(face_x * self.block_size, face_y * self.block_size) + c_relative
         )
 
     def face_if_at_edge(self, c: Coord, d: Direction) -> Optional[FaceOffset]:
         """Return the face the coordinate is on, if move in direction would go off the edge."""
-        x = c.x % self.x_block_size
-        y = c.y % self.y_block_size
+        x = c.x % self.block_size
+        y = c.y % self.block_size
         match d:
             case Direction.UP:
                 if y != 0:
                     return None
             case Direction.RIGHT:
-                if x != self.x_block_size - 1:
+                if x != self.block_size - 1:
                     return None
             case Direction.DOWN:
-                if y != self.y_block_size - 1:
+                if y != self.block_size - 1:
                     return None
             case Direction.LEFT:
                 if x != 0:
                     return None
             case _:
                 assert_never(d)
-        return (c.x // self.x_block_size, c.y // self.y_block_size)
+        return (c.x // self.block_size, c.y // self.block_size)
 
     def move(self, c: Coord, d: Direction) -> tuple[Coord, Direction]:
         """Move one step in given direction.
@@ -198,8 +245,9 @@ class MonkeyMap:
             case None:
                 d_new = d
             case (int(face_x), int(face_y)):
-                (face_new, edge_new, d_new) = self.topology[(face_x, face_y)][d]
-                c_new = self.to_coord(c_new, face_new, edge_new)
+                (face_new, edge_new, preserve) = self.topology[(face_x, face_y)][d]
+                d_new = edge_new.opposite()
+                c_new = self.to_coord(c_new, d, face_new, edge_new, preserve)
         # print(f"Testing move to {c_new} {d_new}, ", end="")
         if self.board[c_new]:
             return c, d
@@ -208,16 +256,17 @@ class MonkeyMap:
     def walk(self) -> int:
         """Walk along given path, return final password.
 
-        >>> m = MonkeyMap(TEST_DATA)
-        >>> m.walk(WRAP_TOPOLOGY)
+        >>> MonkeyMap(TEST_DATA, TEST_WRAP_TOPOLOGY).walk()
         6032
+        >>> MonkeyMap(TEST_DATA, TEST_FOLD_TOPOLOGY).walk()
+        5031
         """
-        c = self.to_coord(
-            Coord(0, 0), (self.x_start // self.x_block_size, 0), Direction.UP
-        )
         d = Direction.RIGHT
+        c = self.to_coord(
+            Coord(0, 0), d, (self.x_start // self.block_size, 0), Direction.UP, True
+        )
         for step in self.path:
-            # print("Step", step)
+ #           print("Step", step, c, d, "->", end="")
             match step:
                 case s if s.isdigit():
                     for _ in range(int(s)):
@@ -228,6 +277,7 @@ class MonkeyMap:
                     d = d.right()
                 case _:
                     raise ValueError("Bad in path")
+#            print(c, d)
         return 1000 * (c.y + 1) + 4 * (c.x + 1) + d.left().value
 
     def show(self) -> None:
@@ -258,9 +308,8 @@ TEST_DATA = """        ...#
 
 
 if __name__ == "__main__":
-    # testmod()
+    testmod()
     # m = MonkeyMap(stdin.read())
-    m = MonkeyMap(TEST_DATA, TEST_WRAP_TOPOLOGY)
-    print(m.walk())
-    m = MonkeyMap(stdin.read(), MAIN_WRAP_TOPOLOGY)
-    print(m.walk())
+    #print(MonkeyMap(TEST_DATA, TEST_WRAP_TOPOLOGY).walk())
+    print(MonkeyMap(stdin.read(), MAIN_WRAP_TOPOLOGY).walk())
+    #print(MonkeyMap(TEST_DATA, TEST_FOLD_TOPOLOGY).walk())
