8cdd64f0894775ad8275f1aee24e2f0bf0a79c7c
Joe Watson <jw120@users.noreply.github.com>
2023-01-15 17:12:55 +0900

    rework on 2022 16

--- python/aoc_2022_16.py
+++ python/aoc_2022_16.py
@@ -1,7 +1,9 @@
-"""Advent of Code 2022 - Day 16."""
+"""Advent of Code 2022 - Day 16.
 
-# TODO
-# Only consider moves which open a valve immediately
+Key insight - dfs where only consider moving to open a valve
+
+"""
+
 
 from __future__ import annotations
 
@@ -10,42 +12,91 @@ from dataclasses import dataclass
 from doctest import testmod
 from re import fullmatch
 from sys import stdin
-from typing import cast
+from typing import cast, Iterable, TypeVar, TypeAlias
+
+
+ValveName: TypeAlias = str
 
 
 @dataclass
 class Valve:
-    """Data class for each valve."""
+    """Data class for each colour valve."""
 
-    name: str
+    name: ValveName
     flow_rate: int
-    tunnels: dict[str, int]  # Exit tunnels and distances
+    tunnels: list[ValveName]  # Exit tunnels
+
+    @staticmethod
+    def read(s: str) -> Valve:
+        """Parse a value from a string.
+
+        >>> Valve.read("Valve BB has flow rate=13; tunnels lead to valves CC, AA")
+        Valve(name='BB', flow_rate=13, tunnels=['AA', 'CC'])
+        """
+        m = fullmatch(
+            r"Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? (.+)", s
+        )
+        assert m, f"read_value match failed on '{s}'"
+        name = m.group(1)
+        flow_rate = int(m.group(2))
+        tunnels = sorted(cast(list[str], m.group(3).split(", ")))
+        assert len(tunnels) > 0
+        return Valve(name, flow_rate, tunnels)
+
+    @staticmethod
+    def read_multi(s: str) -> list[Valve]:
+        """Parse multiple valves from a multi-line string."""
+        return [Valve.read(line.strip()) for line in s.splitlines()]
 
 
-def read_valve(s: str) -> Valve:
-    """Parse a value from a string.
+T = TypeVar("T")
 
-    >>> read_valve("Valve BB has flow rate=13; tunnels lead to valves CC, AA")
-    Valve(name='BB', flow_rate=13, tunnels={'CC': 1, 'AA': 1})
+
+def node_adj_to_dist(adj: dict[T, Iterable[T]], node: T) -> dict[T, int]:
+    """Derive distances for one node for graph from node adjacencies.
+
+    >>> a = {v.name: v.tunnels for v in Valve.read_multi(TEST_DATA)}
+    >>> distances = node_adj_to_dist(a, "AA")
+    >>> [distances[k] for k in sorted(distances.keys())]
+    [0, 1, 2, 1, 2, 3, 4, 5, 1, 2]
     """
-    m = fullmatch(
-        r"Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? (.+)", s
-    )
-    assert m, f"read_value match failed on '{s}'"
-    name = m.group(1)
-    flow_rate = int(m.group(2))
-    tunnels = {s: 1 for s in cast(list[str], m.group(3).split(", "))}
-    assert len(tunnels) > 0
-    return Valve(name, flow_rate, tunnels)
+    distances: dict[T, int] = {}
+    queue: deque[tuple[T, int]] = deque([(node, 0)])
+    while queue:
+        n, d = queue.popleft()
+        distances[n] = d
+        for x in adj[n]:
+            if x not in distances:
+                queue.append((x, d + 1))
+    return distances
+
+
+def graph_adj_to_dist(adj: dict[T, Iterable[T]]) -> dict[tuple[T, T], int]:
+    """Derive distance matrix for graph from node adjacencies.
+
+    >>> a = {v.name: v.tunnels for v in Valve.read_multi(TEST_DATA)}
+    >>> d = graph_adj_to_dist(a)
+    >>> d[("JJ", "HH")]
+    7
+    >>> d[("II", "CC")]
+    3
+    """
+    d: dict[tuple[T, T], int] = {}
+    for n1 in adj.keys():
+        dist1 = node_adj_to_dist(adj, n1)
+        for n2 in adj.keys():
+            d[(n1, n2)] = dist1[n2]
+            d[(n2, n2)] = dist1[n2]
+    return d
 
 
 @dataclass(frozen=True)
 class WalkState:
     """State for depth-first walk of available paths"""
 
-    current_valve_name: str  # Current position
-    unopened: frozenset[str]  # Valves (with non-zero flow) still to be opened
-    path: list[str]
+    current_valve_name: ValveName  # Current position
+    unopened: frozenset[ValveName]  # Valves (with non-zero flow) still to be opened
+    path: list[ValveName]
     time_remaining: int
     score: int  # Flow released over all available time from valves opened so far
 
@@ -54,113 +105,41 @@ class Volcano:
     """Main state for day 16."""
 
     def __init__(self, s: str, start_valve_name: str) -> None:
-        valve_list: list[Valve] = [read_valve(line.strip()) for line in s.splitlines()]
+        valve_list: list[Valve] = [Valve.read(line.strip()) for line in s.splitlines()]
         self.valves: dict[str, Valve] = {v.name: v for v in valve_list}
+        self.distances: dict[tuple[str, str], int] = graph_adj_to_dist(
+            {v.name: v.tunnels for v in valve_list}
+        )
         self.non_zero_valve_names: frozenset[str] = frozenset(
             v.name for v in self.valves.values() if v.flow_rate > 0
         )
-        self.start_valve = self.valves[start_valve_name]
-        self.stack: deque[WalkState] = deque()
-        self.distances: dict[tuple[str, str], int] = {}
-        updated = True
-        while updated:
-            updated = False
-            for v_name in self.valves.keys():
-                self.distances[(v_name, v_name)] = 0
-                for t_name, t_distance in self.valves[v_name].tunnels.items():
-                    for in_name in self.valves.keys():
-                        if (in_name, v_name) in self.distances:
-                            new_distance: int = (
-                                self.distances[(in_name, v_name)] + t_distance
-                            )
-                            if (in_name, t_name) in self.distances:
-                                old_distance = self.distances[(in_name, t_name)]
-                                if old_distance <= new_distance:
-                                    continue
-                            updated = True
-                            self.distances[(in_name, t_name)] = new_distance
-                            self.distances[(t_name, in_name)] = new_distance
-        for v1 in self.valves.keys():
-            for v2 in self.valves.keys():
-                print(v1, v2, self.distances[(v1, v2)])
+        self.start_valve_name: str = start_valve_name
 
-    def push_move(
-        self, state: WalkState, tunnel: str, distance: int, threshold_score: int
-    ) -> None:
-        """Push state that would be reached after moving to given tunnel from state."""
+    def solve(self, time_available: int) -> int:
+        """Find best scoring path.
 
-        assert state.time_remaining >= distance, f"Time up for move at {state}"
-        #        if len(state.path) > 1 and tunnel == state.path[-2]:
-        # print("Skipped double-back to", tunnel, "from", state.path)
-        #            return
-        possible_score = state.score + (state.time_remaining - distance) * sum(
-            self.valves[v].flow_rate for v in state.unopened
-        )
-        if possible_score > threshold_score:
-            new_path = state.path.copy()
-            new_path.append(tunnel)
-            self.stack.append(
-                WalkState(
-                    tunnel,
-                    state.unopened,
-                    new_path,
-                    state.time_remaining - distance,
-                    state.score,
-                )
-            )
+        Depth-first search using th adjacency matrix.
 
-    def push_open(self, state: WalkState, threshold_score: int) -> None:
-        """Push state that would be reached by opening value from state."""
-        if state.current_valve_name not in state.unopened:
-            return
-        # assert state.current_valve_name in state.unopened, f"Not unopened at {state}"
-        assert state.time_remaining > 0, f"Time up for open at {state}"
-        possible_score = state.score + (state.time_remaining - 1) * sum(
-            self.valves[v].flow_rate for v in state.unopened
-        )
-        # print(
-        #     "Considering open",
-        #     state.current_valve_name,
-        #     state.score,
-        #     possible_score,
-        #     threshold_score,
-        #     state.unopened,
-        # )
-        if possible_score > threshold_score:
-            flow_rate = self.valves[state.current_valve_name].flow_rate
-            assert flow_rate > 0
-            extra_score = flow_rate * (state.time_remaining - 1)
-            new_path = state.path.copy()
-            new_path.append("OPEN")
-            self.stack.append(
-                WalkState(
-                    state.current_valve_name,
-                    state.unopened - frozenset([state.current_valve_name]),
-                    new_path,
-                    state.time_remaining - 1,
-                    state.score + extra_score,
-                )
-            )
+        >>> Volcano(TEST_DATA, "AA").solve(30)
+        1651
+        """
+        stack: deque[WalkState] = deque([])
 
-    def generate_paths(self, time_available: int) -> int:
-        """Generate all possible paths, returning the best score"""
-
-        self.stack.append(
+        stack.append(
             WalkState(
-                self.start_valve.name,
-                self.non_zero_valve_names,
-                [self.start_valve.name],
-                time_available,
-                0,
+                current_valve_name=self.start_valve_name,
+                unopened=self.non_zero_valve_names,
+                path=[],
+                time_remaining=time_available,
+                score=0,
             )
         )
         best_score: int = 0
 
         while True:
-            if not self.stack:
+            if not stack:
                 break
-            state = self.stack.pop()
-            # print("Popped", state.score, state.path, state.unopened)
+            state = stack.pop()
             if state.score > best_score:
                 best_score = state.score
                 print("Best", state.score, state.path)
@@ -168,9 +147,20 @@ class Volcano:
                 if destination == state.current_valve_name:
                     continue
                 distance = self.distances[state.current_valve_name, destination]
-                if distance < state.time_remaining:
-                    self.push_move(state, destination, distance, best_score)
-            self.push_open(state, best_score)
+                new_time = state.time_remaining - distance - 1
+                if new_time > 0:
+                    new_score = (
+                        state.score + self.valves[destination].flow_rate * new_time
+                    )
+                    stack.append(
+                        WalkState(
+                            current_valve_name=destination,
+                            unopened=state.unopened - frozenset([destination]),
+                            path=state.path + [destination],
+                            time_remaining=state.time_remaining - distance - 1,
+                            score=new_score,
+                        )
+                    )
         return best_score
 
 
@@ -188,6 +178,5 @@ Valve JJ has flow rate=21; tunnel leads to valve II"""
 
 if __name__ == "__main__":
     testmod()
-    # v = Volcano(TEST_DATA, "AA")
     v = Volcano(stdin.read(), "AA")
-    print(v.generate_paths(30))
+    print(v.solve(30))
