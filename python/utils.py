c10cdf1cbb4a068d9bb1cadc4f9d7ac26edf44b7
Joe Watson <jw120@users.noreply.github.com>
2023-01-17 18:19:37 +0900

    Fixed 2020 06

--- python/utils.py
+++ python/utils.py
@@ -15,6 +15,11 @@ def set_union(xs: Iterable[set[T]]) -> set[T]:
     """Combine sets by union.
 
     Provided as using set.union(*xs) does not type-check.
+
+    >>> sorted(set_union([{1, 2, 3}, {2, 3, 4}, {5, 1}]))
+    [1, 2, 3, 4, 5]
+    >>> set_union([])
+    set()
     """
     x: set[T] = set()
     return x.union(*xs)
@@ -24,9 +29,17 @@ def set_intersection(xs: Iterable[set[T]]) -> set[T]:
     """Combine sets by union.
 
     Provided as using set.intersection(*xs) does not type-check.
+
+    >>> sorted(set_intersection([{1, 2, 3, 4}, {2, 3, 4, 5}, {2, 4, 6, 8}]))
+    [2, 4]
+    >>> set_intersection([])
+    set()
     """
-    x: set[T] = set()
-    return x.intersection(*xs)
+    try:
+        x: set[T] = next(xs.__iter__())
+        return x.intersection(*xs)
+    except StopIteration:
+        return set()
 
 
 def sign(x: int) -> int:
@@ -36,3 +49,9 @@ def sign(x: int) -> int:
     if x < 0:
         return -1
     return 0
+
+
+if __name__ == "__main__":
+    from doctest import testmod
+
+    testmod()
