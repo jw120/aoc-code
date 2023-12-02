db553d5bc5cf27d818119e1408c68101ea6423fc
Joe Watson <jw120@users.noreply.github.com>
2023-12-02 23:35:24 +0800

    2023.01 done

---
+++ python/aoc_2023_01.py
@@ -0,0 +1,69 @@
+"""Advent of Code 2023 - Day 1."""
+
+from doctest import testmod
+from sys import stdin
+from typing import Final
+
+
+def calibration(s: str) -> int:
+    """Return first and last digit on the line as an integer
+
+    >>> calibration("pqr3stu8vwx")
+    38
+    >>> calibration("treb7uchet")
+    77
+    """
+    digits: list[int] = [int(c) for c in s if c.isdigit()]
+    return digits[0] * 10 + digits[-1]
+
+
+WORD_DIGITS: Final[list[str]] = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+]
+
+
+def starts_num_word(s: str) -> list | int:
+    """If string starts with a digit or its word version, return it.
+
+    >>> starts_num_word("2qw")
+    2
+    >>> starts_num_word("eight23")
+    8
+    >>> starts_num_word("a2") is None
+    True
+    """
+    if s[0].isdigit():
+        return int(s[0])
+    for i, w in zip(range(1, 10), WORD_DIGITS, strict=True):
+        if s.startswith(w):
+            return i
+    return None
+
+
+def calibration_word(s: str) -> int:
+    """Return first and last digit on the line as an integer allowing word digits.
+
+    >>> calibration_word("two1nine")
+    29
+    >>> calibration_word("7pqrstsixteen")
+    76
+    """
+    digits: list[int] = [
+        x for x in [starts_num_word(s[i:]) for i in range(0, len(s))] if x is not None
+    ]
+    return digits[0] * 10 + digits[-1]
+
+
+if __name__ == "__main__":
+    testmod()
+    lines: list[str] = list(stdin.readlines())
+    print(sum(calibration(s) for s in lines))
+    print(sum(calibration_word(s) for s in lines))
