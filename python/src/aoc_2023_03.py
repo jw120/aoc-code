abd81589620e538be7be70ba908fbc71fe11173a
Joe Watson <jw120@users.noreply.github.com>
2023-12-03 16:09:01 +0800

    2023 03 done

--- python/src/aoc_2023_03.py
+++ python/src/aoc_2023_03.py
@@ -21,20 +21,25 @@ class Schematic:
     """Main class to hold schematic"""
 
     def __init__(self, lines: list[str]) -> None:
+        # Read grid data
         self.extent: Extent = Extent(x=len(lines[0]), y=len(lines))
         self.data: dict[Coord, str] = {}
         for y, line in enumerate(lines):
             for x, c in enumerate(line):
                 self.data[Coord(x, y)] = c
-
-    def numbers(self) -> Iterator[Number]:
-        """Yield all the numbers."""
+        # Locate symbols
+        self.symbols: list[Coord] = []
+        for c in self.extent.upto():
+            if (not self.data[c].isdigit()) and self.data[c] != ".":
+                self.symbols.append(c)
+        # Locate numbers
+        self.numbers: list[Number] = []
         n: Number | None = None
         for y in range(self.extent.y):
             for x in range(self.extent.x + 1):
                 if x == self.extent.x:
                     if n is not None:
-                        yield n
+                        self.numbers.append(n)
                     n = None
                 else:
                     c = Coord(x, y)
@@ -47,39 +52,37 @@ class Schematic:
                             n.length += 1
                     else:
                         if n is not None:
-                            yield n
+                            self.numbers.append(n)
                         n = None
 
-    def is_symbol(self, c: Coord) -> bool:
-        """Is the given coordinate a symbol"""
-        return not (self.data[c].isdigit() or self.data[c] == ".")
+    def gear_ratios(self) -> Iterator[int]:
+        """For each star adjacent to two numbers, yield their product"""
+        for c in self.extent.upto():
+            if self.data[c] == "*":
+                adjacent_numbers = [
+                    n.value for n in self.numbers if self.is_adjacent(n, c)
+                ]
+                if len(adjacent_numbers) == 2:
+                    yield adjacent_numbers[0] * adjacent_numbers[1]
 
-    def is_symbol_adjacent(self, n: Number) -> bool:
-        """Is the number adjacent to a symbol on the grid?"""
-        if n.position.y > 0:
-            for x in range(
-                max(0, n.position.x - 1),
-                min(self.extent.x, n.position.x + n.length + 1),
-            ):
-                if self.is_symbol(Coord(x, n.position.y - 1)):
-                    return True
-        if n.position.y < self.extent.y - 1:
-            for x in range(
-                max(0, n.position.x - 1),
-                min(self.extent.x, n.position.x + n.length + 1),
-            ):
-                if self.is_symbol(Coord(x, n.position.y + 1)):
-                    return True
-        if n.position.x > 0 and self.is_symbol(Coord(n.position.x - 1, n.position.y)):
-            return True
-        if n.position.x + n.length < self.extent.x and self.is_symbol(
-            Coord(n.position.x + n.length, n.position.y)
-        ):
-            return True
-        return False
+    def is_adjacent(self, n: Number, c: Coord) -> bool:
+        """Is the number adjacent to the given coordinate"""
+        return (
+            c.x >= n.position.x - 1
+            and c.x <= n.position.x + n.length
+            and c.y >= n.position.y - 1
+            and c.y <= n.position.y + 1
+        )
 
 
 if __name__ == "__main__":
     testmod()
     schematic = Schematic([s.strip() for s in stdin.readlines()])
-    print(sum(n.value for n in schematic.numbers() if schematic.is_symbol_adjacent(n)))
+    print(
+        sum(
+            n.value
+            for n in schematic.numbers
+            if any(schematic.is_adjacent(n, s) for s in schematic.symbols)
+        )
+    )
+    print(sum(schematic.gear_ratios()))
