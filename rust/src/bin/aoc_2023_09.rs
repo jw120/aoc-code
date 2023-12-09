58699377ea609e32f5869d7d4deb7b8a8fdb1d2b
Joe Watson <jw120@users.noreply.github.com>
2023-12-09 14:13:30 +0800

    opt 2023 09

--- rust/src/bin/aoc_2023_09.rs
+++ rust/src/bin/aoc_2023_09.rs
@@ -8,29 +8,25 @@ fn parse_sequence(s: &str) -> Vec<i64> {
         .collect()
 }
 
-fn extrapolate(sequence: &[i64]) -> i64 {
+fn extrapolate(sequence: &[i64], front: bool) -> i64 {
     if sequence.iter().all(|x| *x == 0) {
         0
     } else {
         let sub_sequence: Vec<i64> = sequence.windows(2).map(|w| w[1] - w[0]).collect();
-        sequence.last().unwrap() + extrapolate(&sub_sequence)
-    }
-}
-
-fn extrapolate_front(sequence: &[i64]) -> i64 {
-    if sequence.iter().all(|x| *x == 0) {
-        0
-    } else {
-        let sub_sequence: Vec<i64> = sequence.windows(2).map(|w| w[1] - w[0]).collect();
-        sequence.first().unwrap() - extrapolate_front(&sub_sequence)
+        let sub_value = extrapolate(&sub_sequence, front);
+        if front {
+            sequence.first().unwrap() - sub_value
+        } else {
+            sequence.last().unwrap() + sub_value
+        }
     }
 }
 
 fn main() {
     let sequences: Vec<Vec<i64>> = stdin_lines().map(|s| parse_sequence(&s)).collect();
 
-    let part_a: i64 = sequences.iter().map(|seq| extrapolate(seq)).sum();
-    let part_b: i64 = sequences.iter().map(|seq| extrapolate_front(seq)).sum();
+    let part_a: i64 = sequences.iter().map(|seq| extrapolate(seq, false)).sum();
+    let part_b: i64 = sequences.iter().map(|seq| extrapolate(seq, true)).sum();
 
     println!("{}", part_a);
     println!("{}", part_b);
