0d4cb910fed1b6cca3cf1cb9d5e7d87807bb54f7
Joe Watson <jw120@users.noreply.github.com>
2023-12-25 20:25:49 +0900

    Work on 2023 19

--- rust/src/bin/aoc_2023_19.rs
+++ rust/src/bin/aoc_2023_19.rs
@@ -3,6 +3,10 @@
 use aoc_rust::stdin_lines;
 use std::collections::HashMap;
 
+//
+// Data structures
+//
+
 #[derive(Debug)]
 struct Workflow {
     rules: Vec<Rule>,
@@ -63,6 +67,9 @@ fn parse_rule(s: &str) -> Rule {
     }
 }
 
+//
+// Part (a) data structure - Parts
+
 #[derive(Debug)]
 struct Part {
     x: usize,
@@ -105,31 +112,9 @@ fn parse_part(s: &str) -> Part {
     Part { x, m, a, s }
 }
 
-fn read_input() -> (HashMap<String, Workflow>, Vec<Part>) {
-    let mut h: HashMap<String, Workflow> = HashMap::new();
-    let mut ps: Vec<Part> = Vec::new();
-    let mut parsing_workflows: bool = true;
-
-    for line in stdin_lines() {
-        if line.is_empty() {
-            assert!(parsing_workflows);
-            parsing_workflows = false;
-            continue;
-        }
-        if parsing_workflows {
-            let open_brace: usize = line.find('{').unwrap();
-            let name: &str = &line[..open_brace];
-            let contents: &str = line[open_brace + 1..].strip_suffix('}').unwrap();
-            let last_comma: usize = contents.rfind(',').unwrap();
-            let default: Destination = parse_destination(&contents[last_comma + 1..]);
-            let rules: Vec<Rule> = contents[..last_comma].split(',').map(parse_rule).collect();
-            h.insert(name.to_string(), Workflow { rules, default });
-        } else {
-            ps.push(parse_part(&line));
-        }
-    }
-    (h, ps)
-}
+//
+// Part (a) logic
+//
 
 fn apply(rule: &Rule, part: &Part) -> Option<Destination> {
     let value = match rule.category {
@@ -164,10 +149,186 @@ fn run_part(part: &Part, workflows: &HashMap<String, Workflow>) -> Option<usize>
     }
 }
 
+//
+// Part (b) logic
+//
+
+// Possible part values (inclusive)
+#[derive(Clone, Debug)]
+struct Set {
+    x: (usize, usize),
+    m: (usize, usize),
+    a: (usize, usize),
+    s: (usize, usize),
+}
+
+fn overlap(s1: &Set, s2: &Set) -> bool {
+    range_overlap(s1.x, s2.x)
+        || range_overlap(s1.m, s2.m)
+        || range_overlap(s1.a, s2.a)
+        || range_overlap(s1.s, s2.s)
+}
+
+fn range_overlap((a_min, a_max): (usize, usize), (b_min, b_max): (usize, usize)) -> bool {
+    !(a_max < b_min || b_max < a_min)
+}
+
+// Split set based on given rule - returning sets that pass, fail
+fn split(rule: &Rule, set: Set) -> (Option<Set>, Option<Set>) {
+    let (value_min, value_max) = match rule.category {
+        Category::X => set.x,
+        Category::M => set.m,
+        Category::A => set.a,
+        Category::S => set.s,
+    };
+    let limit = rule.limit;
+    let (pass, fail) = if rule.greater {
+        if limit > value_max {
+            // None of the values pass
+            (None, Some((value_min, value_max)))
+        } else if limit > value_min {
+            // Some values pass
+            (Some((limit, value_max)), Some((value_min, limit - 1)))
+        } else {
+            // All values pass
+            (Some((value_min, value_max)), None)
+        }
+    } else if limit < value_min {
+        // None of the values pass
+        (None, Some((value_min, value_max)))
+    } else if limit < value_max {
+        // Some values pass
+        (Some((value_min, limit - 1)), Some((limit, value_max)))
+    } else {
+        // All values pass
+        (Some((value_min, value_max)), None)
+    };
+    match rule.category {
+        Category::X => (
+            pass.map(|p| Set { x: p, ..set }),
+            fail.map(|p| Set { x: p, ..set }),
+        ),
+        Category::M => (
+            pass.map(|p| Set { m: p, ..set }),
+            fail.map(|p| Set { m: p, ..set }),
+        ),
+        Category::A => (
+            pass.map(|p| Set { a: p, ..set }),
+            fail.map(|p| Set { a: p, ..set }),
+        ),
+        Category::S => (
+            pass.map(|p| Set { s: p, ..set }),
+            fail.map(|p| Set { s: p, ..set }),
+        ),
+    }
+}
+
+fn run_set(set: Set, workflow_name: String, workflows: &HashMap<String, Workflow>) -> Vec<Set> {
+    let mut accepted: Vec<Set> = Vec::new();
+    let workflow = workflows.get(&workflow_name).unwrap();
+    let mut remaining_set: Set = set;
+    let mut nothing_remaining: bool = false;
+    for rule in workflow.rules.clone() {
+        let (pass, fail) = split(&rule, remaining_set.clone());
+        if let Some(pass_set) = pass {
+            match rule.destination {
+                Destination::Accept => accepted.push(pass_set),
+                Destination::Reject => {}
+                Destination::Workflow(w) => {
+                    for x in run_set(pass_set, w, workflows) {
+                        accepted.push(x);
+                    }
+                }
+            }
+        }
+        if let Some(fail_set) = fail {
+            remaining_set = fail_set;
+        } else {
+            nothing_remaining = true;
+        }
+    }
+    if !nothing_remaining {
+        match &workflow.default {
+            Destination::Accept => accepted.push(remaining_set),
+            Destination::Reject => {}
+            Destination::Workflow(w) => {
+                for x in run_set(remaining_set, w.to_string(), workflows) {
+                    accepted.push(x);
+                }
+            }
+        }
+    }
+    accepted
+}
+
+fn set_value(set: &Set) -> usize {
+    assert!(set.x.1 >= set.x.0);
+    assert!(set.m.1 >= set.m.0);
+    assert!(set.a.1 >= set.a.0);
+    assert!(set.s.1 >= set.s.0);
+    (set.x.1 - set.x.0 + 1)
+        * (set.m.1 - set.m.0 + 1)
+        * (set.a.1 - set.a.0 + 1)
+        * (set.s.1 - set.s.0 + 1)
+}
+
+fn sum_valid(valid: &[Set]) -> usize {
+    println!("Valid {:?}", valid);
+    for s in valid {
+        println!("{:?}", s);
+    }
+    for s1 in valid {
+        for s2 in valid {
+            assert!(!overlap(s1, s2), "{:?} {:?}", s1, s2);
+        }
+    }
+
+    valid.iter().map(set_value).sum()
+}
+
+//
+// Top-level code
+//
+
+fn read_input() -> (HashMap<String, Workflow>, Vec<Part>) {
+    let mut h: HashMap<String, Workflow> = HashMap::new();
+    let mut ps: Vec<Part> = Vec::new();
+    let mut parsing_workflows: bool = true;
+
+    for line in stdin_lines() {
+        if line.is_empty() {
+            assert!(parsing_workflows);
+            parsing_workflows = false;
+            continue;
+        }
+        if parsing_workflows {
+            let open_brace: usize = line.find('{').unwrap();
+            let name: &str = &line[..open_brace];
+            let contents: &str = line[open_brace + 1..].strip_suffix('}').unwrap();
+            let last_comma: usize = contents.rfind(',').unwrap();
+            let default: Destination = parse_destination(&contents[last_comma + 1..]);
+            let rules: Vec<Rule> = contents[..last_comma].split(',').map(parse_rule).collect();
+            h.insert(name.to_string(), Workflow { rules, default });
+        } else {
+            ps.push(parse_part(&line));
+        }
+    }
+    (h, ps)
+}
+
 fn main() {
     let (workflows, parts) = read_input();
 
     let part_a: usize = parts.iter().filter_map(|p| run_part(p, &workflows)).sum();
-
     println!("{part_a}");
+
+    let start: Set = Set {
+        x: (1, 4000),
+        m: (1, 4000),
+        a: (1, 4000),
+        s: (1, 4000),
+    };
+    let valid: Vec<Set> = run_set(start, "in".to_string(), &workflows);
+    let part_b: usize = sum_valid(&valid);
+    println!("{part_b}");
 }
