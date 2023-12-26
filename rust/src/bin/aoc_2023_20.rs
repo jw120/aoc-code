63dcd2b8c10bbdeb661eff9ac350129ac82f3ea4
Joe Watson <jw120@users.noreply.github.com>
2023-12-26 23:57:26 +0900

    work on 2023 20a

--- rust/src/bin/aoc_2023_20.rs
+++ rust/src/bin/aoc_2023_20.rs
@@ -2,8 +2,9 @@
 
 use aoc_rust::stdin_lines;
 use std::collections::HashMap;
+use std::collections::VecDeque;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum ModuleType {
     Broadcaster,
     FlipFlop,
@@ -12,14 +13,35 @@ enum ModuleType {
 
 type Module = (ModuleType, Vec<String>);
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum Pulse {
+    Low,
+    High,
+}
+
+impl Pulse {
+    fn show(self) -> &'static str {
+        match self {
+            Pulse::High => "high",
+            Pulse::Low => "low",
+        }
+    }
+}
+
 fn parse_module(s: &str) -> (String, Module) {
     let mut arrow_iter = s.split(" -> ");
-    let module_name = arrow_iter.next().unwrap();
-    let module_type = match module_name.chars().next().unwrap() {
+    let module_key = arrow_iter.next().unwrap();
+    let module_type = match module_key.chars().next().unwrap() {
         'b' => ModuleType::Broadcaster,
         '%' => ModuleType::FlipFlop,
         '&' => ModuleType::Conjunction,
         ch => panic!("Unknown module type character '{ch}'"),
+    };
+    let module_name = if module_type == ModuleType::Broadcaster {
+        assert_eq!(module_key, "broadcaster");
+        module_key
+    } else {
+        &module_key[1..]
     };
     let destinations: Vec<String> = arrow_iter
         .next()
@@ -30,12 +52,73 @@ fn parse_module(s: &str) -> (String, Module) {
     (module_name.to_string(), (module_type, destinations))
 }
 
+fn run(modules: &HashMap<String, Module>) {
+    // Set up states
+    let mut flipflop_states: HashMap<String, bool> = HashMap::new();
+    let mut conjunction_states: HashMap<String, HashMap<String, Pulse>> = HashMap::new();
+    for (module_name, (module_type, _outputs)) in modules {
+        match module_type {
+            ModuleType::FlipFlop => {
+                flipflop_states.insert(module_name.to_string(), false);
+            }
+            ModuleType::Conjunction => {
+                conjunction_states.insert(module_name.to_string(), HashMap::new());
+            }
+            ModuleType::Broadcaster => {}
+        }
+    }
+    println!("Flipflops {:?}", flipflop_states);
+    for (module_name, (_module_type, outputs)) in modules {
+        for output in outputs {
+            if let Some((connection_type, _connection_outputs)) = modules.get(output) {
+                if *connection_type == ModuleType::Conjunction {
+                    let conjunction_map = conjunction_states.get_mut(output).unwrap();
+                    conjunction_map.insert(module_name.to_string(), Pulse::Low);
+                }
+            }
+        }
+    }
+    println!("Conjunctions {:?}", conjunction_states);
+
+    // Set up queue to hold pulses
+    let mut pulse_queue: VecDeque<(Pulse, String, String)> =
+        VecDeque::from([(Pulse::Low, "button".to_string(), "broadcaster".to_string())]);
+    while let Some((pulse, source, destination)) = pulse_queue.pop_front() {
+        println!("{} -{}-> {}", source, pulse.show(), destination);
+        let (module_type, outputs) = modules.get(&destination).unwrap();
+        match module_type {
+            ModuleType::Broadcaster => {
+                for output in outputs {
+                    pulse_queue.push_back((pulse, destination.clone(), output.to_string()));
+                }
+            }
+            ModuleType::FlipFlop => {
+                if pulse == Pulse::Low {
+                    let state = flipflop_states.get_mut(&destination).unwrap();
+                    let output_pulse = if *state { Pulse::Low } else { Pulse::High };
+                    *state = !*state;
+                    for output in outputs {
+                        pulse_queue.push_back((
+                            output_pulse,
+                            destination.clone(),
+                            output.to_string(),
+                        ));
+                    }
+                }
+            }
+            ModuleType::Conjunction => {}
+        }
+    }
+}
+
 fn main() {
     let mut modules: HashMap<String, Module> = HashMap::new();
     for line in stdin_lines() {
         let (module_name, module) = parse_module(&line);
         modules.insert(module_name, module);
     }
+    println!("Modules {:?}", modules);
+    run(&modules);
 
     let part_a: usize = modules.len();
 
