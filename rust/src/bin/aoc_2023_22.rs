e059d672506658dc184664e373ee80cc7682962d
Joe Watson <jw120@users.noreply.github.com>
2024-01-01 22:42:46 +0900

    more 2023 22

--- rust/src/bin/aoc_2023_22.rs
+++ rust/src/bin/aoc_2023_22.rs
@@ -1,22 +1,21 @@
 // Advent of Code, 2023 day 22
 
-use aoc_rust::stdin_lines;
-use aoc_rust::{ucoord3, UCoord3};
-use std::cmp::max;
+use aoc_rust::{stdin_lines, ucoord3, UCoord3};
+use std::{cmp::max, collections::HashSet};
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 enum Direction {
     X,
     Y,
     Z,
 }
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 struct Brick {
     start: UCoord3, // smaller coordinate end
     end: UCoord3,   // larger coordinate end
     direction: Direction,
-    length: usize, // difference to other end
+    length: usize,
 }
 
 impl Brick {
@@ -59,6 +58,7 @@ impl Brick {
 
 // Vector to hold which brick (index) occupies each space
 struct Occupancies {
+    bricks: Vec<Brick>,
     pos: Vec<Option<usize>>,
     // x_max: usize,
     // y_max: usize,
@@ -68,32 +68,73 @@ struct Occupancies {
 }
 
 impl Occupancies {
-    fn new(x_max: usize, y_max: usize, z_max: usize) -> Occupancies {
-        let pos: Vec<Option<usize>> = vec![None; x_max * y_max * z_max];
-        let y_factor: usize = 1 + x_max;
-        let z_factor: usize = y_factor * (1 + y_max);
+    // x runs 0..x_size, y 0..y_size, z 1..=z_size
+    fn new() -> Occupancies {
+        let bricks: Vec<Brick> = stdin_lines().map(read_brick).collect();
+
+        let mut x_max: usize = 0;
+        let mut y_max: usize = 0;
+        let mut z_max: usize = 0;
+        for brick in &bricks {
+            x_max = max(x_max, brick.end.x);
+            y_max = max(y_max, brick.end.y);
+            z_max = max(z_max, brick.end.z);
+        }
+        let x_size = 1 + x_max;
+        let y_size = 1 + y_max;
+        let z_size = z_max;
+
+        let pos: Vec<Option<usize>> = vec![None; x_size * y_size * z_size];
+        let y_factor: usize = x_size;
+        let z_factor: usize = x_size * y_size;
+
         Occupancies {
+            bricks,
             pos,
-            // x_max,
-            // y_max,
-            // z_max,
             y_factor,
             z_factor,
         }
     }
 
+    fn build(&mut self) {
+        for brick_index in 0..self.bricks.len() {
+            let brick = self.bricks[brick_index];
+            brick.for_each(|pos| self.set(&pos, Some(brick_index)));
+        }
+    }
+
+    fn offset(&self, position: &UCoord3) -> usize {
+        position.x + position.y * self.y_factor + (position.z - 1) * self.z_factor
+    }
+
     fn get(&self, position: &UCoord3) -> &Option<usize> {
-        let offset = position.x + position.y * self.y_factor + (position.z - 1) * self.z_factor;
-        self.pos.get(offset).unwrap()
+        self.pos.get(self.offset(position)).unwrap()
     }
 
     fn set(&mut self, position: &UCoord3, value: Option<usize>) {
-        let offset = position.x + position.y * self.y_factor + (position.z - 1) * self.z_factor;
+        let offset = self.offset(position);
         println!(
             "Set {} {} {} = {} -> {:?}",
             position.x, position.y, position.z, offset, value
         );
-        *self.pos.get_mut(offset).unwrap() = value;
+        *(self.pos.get_mut(offset).unwrap()) = value;
+    }
+
+    // Return indices of the bricks that are supporting the given brick
+    // and indicate if the given brick rests on the floor
+    fn supported_by(&self, brick: &Brick) -> (HashSet<usize>, bool) {
+        let mut support_set: HashSet<usize> = HashSet::new();
+        let mut on_floor: bool = false;
+        brick.for_each(|position| {
+            if position.z == 1 {
+                on_floor = true;
+            } else if let Some(b) = self.get(&(position - ucoord3(0, 0, 1))) {
+                if self.bricks[*b] != *brick {
+                    support_set.insert(*b);
+                }
+            }
+        });
+        (support_set, on_floor)
     }
 }
 
@@ -114,42 +155,12 @@ fn read_brick<S: AsRef<str>>(s: S) -> Brick {
     Brick::new(read_ucoord3(a), read_ucoord3(b))
 }
 
-fn run(bricks: &[Brick]) {
-    let mut x_max: usize = 0;
-    let mut y_max: usize = 0;
-    let mut z_max: usize = 0;
-    for brick in bricks {
-        x_max = max(x_max, brick.end.x);
-        y_max = max(y_max, brick.end.y);
-        z_max = max(z_max, brick.end.z);
-    }
-    println!("Extent {x_max} {y_max} {z_max}");
-
-    let mut occupancies: Occupancies = Occupancies::new(x_max, y_max, z_max);
-    for (brick_index, brick) in bricks.iter().enumerate() {
-        brick.for_each(|pos| occupancies.set(&pos, Some(brick_index)));
-    }
-
-    for z in 0..=z_max {
-        for x in 0..=x_max {
-            for y in 0..=y_max {
-                if let Some(brick_index) = occupancies.get(&ucoord3(x, y, z)) {
-                    println!("({x}, {y}, {z}): {brick_index}");
-                }
-            }
-        }
-    }
-}
-
 fn main() {
-    let bricks: Vec<Brick> = stdin_lines().map(read_brick).collect();
+    let mut occupancies: Occupancies = Occupancies::new();
+    occupancies.build();
 
-    for (i, brick) in bricks.iter().enumerate() {
-        println!("{i}: {brick:?}");
+    println!("Supported by");
+    for (brick_index, brick) in occupancies.bricks.iter().enumerate() {
+        println!("{}: {:?}", brick_index, occupancies.supported_by(brick));
     }
-    run(&bricks);
-
-    let part_a: usize = 0;
-
-    println!("{part_a}");
 }
