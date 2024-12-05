d44386d75d5a16a94b8f5e03de2465551f592066
Joe Watson <jw120@users.noreply.github.com>
2024-12-05 21:04:06 +0800

    2024 05 python

---
+++ python/src/aoc_2024_05.py
@@ -0,0 +1,46 @@
+"""Advent of Code 2024 - Day 5."""
+
+from itertools import combinations
+from sys import stdin
+
+
+def read_pages() -> tuple[set[tuple[int, int]], list[list[int]]]:
+    """Read page orderings and updates."""
+    orderings: set[tuple[int, int]] = set()
+    updates: list[list[int]] = []
+    in_orderings = True
+    for line in (s.strip() for s in stdin.readlines()):
+        if not line:
+            in_orderings = False
+            continue
+        if in_orderings:
+            a, b = [int(s) for s in line.split("|")]
+            orderings.add((a, b))
+        else:
+            updates.append([int(s) for s in line.split(",")])
+    return (orderings, updates)
+
+
+def valid_middle(update: list[int], orderings: set[tuple[int, int]]) -> int:
+    """Return middle page if update is in valid order, zero otherwise."""
+    for x, y in combinations(update, 2):
+        if (y, x) in orderings:
+            return 0
+    return update[len(update) // 2]
+
+
+def fixed_middle(update: list[int], orderings: set[tuple[int, int]]) -> int:
+    """Return middle page after fixing if the update was invalid, zero otherwise."""
+    if valid_middle(update, orderings) != 0:
+        return 0
+    for i, j in combinations(range(len(update)), 2):
+        x, y = update[i], update[j]
+        if (y, x) in orderings:
+            update[i], update[j] = update[j], update[i]
+    return update[len(update) // 2]
+
+
+if __name__ == "__main__":
+    orderings, updates = read_pages()
+    print(sum(valid_middle(u, orderings) for u in updates))
+    print(sum(fixed_middle(u, orderings) for u in updates))
