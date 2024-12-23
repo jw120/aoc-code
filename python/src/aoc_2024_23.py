ce182efbdca761ea0e8808d2ea07b613adcff9ac
Joe Watson <jw120@users.noreply.github.com>
2024-12-23 15:44:07 +0900

    Done 2024 23

--- python/src/aoc_2024_23.py
+++ python/src/aoc_2024_23.py
@@ -30,6 +30,28 @@ def triples(connections: dict[str, set[str]]) -> set[tuple[str, ...]]:
     return found
 
 
+def largest_group(connections: dict[str, set[str]]) -> set[str]:
+    """Return computers in the largest group with all connected."""
+    unassigned_computers: set[str] = set(connections.keys())
+    largest_set: set[str] = set()
+    while unassigned_computers:
+        frontier: set[str] = {unassigned_computers.pop()}
+        visited: set[str] = set()
+        new_set: set[str] = set()
+        while frontier:
+            x = frontier.pop()
+            visited.add(x)
+            if all(y in connections[x] for y in new_set):
+                new_set.add(x)
+            for y in connections[x]:
+                if y not in new_set and y not in visited:
+                    frontier.add(y)
+        if len(new_set) > len(largest_set):
+            largest_set = new_set.copy()
+    return largest_set
+
+
 if __name__ == "__main__":
     connections = parse(stdin.readlines())
     print(sum(any(s.startswith("t") for s in triple) for triple in triples(connections)))
+    print(",".join(sorted(largest_group(connections))))
