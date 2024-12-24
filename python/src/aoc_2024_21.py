534606158b8e329722a5df0e65e27d845649531f
Joe Watson <jw120@users.noreply.github.com>
2024-12-24 18:39:23 +0900

    2024 21 more

--- python/src/aoc_2024_21.py
+++ python/src/aoc_2024_21.py
@@ -27,22 +27,31 @@ DPAD: Final[dict[str, Coord]] = {
     ">": Coord(2, 1),
 }
 
+# We represent moves of the directional keypad by strings
+type Moves = str
 
-def numeric(s: str) -> list[list[str]]:
+# When there are more than one alternative moves we return a list of moves
+type MoveChoice = list[str]
+
+# We generate move chains which are a series of move sequences (with alternatives)
+type MoveChain = list[MoveChoice]
+
+
+def numeric(s: str) -> MoveChain:
     """Return directional keypad moves for robot to type given sequence on the numeric keypad.
 
     Output is a list of alternatives.
     """
     current: Coord = KEYPAD["A"]
-    output: list[list[str]] = []
+    output: MoveChain = []
     for target in s:
-        moves: list[str] = numeric_moves(current, KEYPAD[target])
+        moves: str | list[str] = numeric_moves(current, KEYPAD[target])
         output.append([m + "A" for m in moves])
         current = KEYPAD[target]
     return output
 
 
-def numeric_moves(start: Coord, end: Coord) -> list[str]:
+def numeric_moves(start: Coord, end: Coord) -> MoveChoice:
     """Return directional moves request to move arm for numeric pad. Avoids gap."""
     # If start at 0 and move to left-most column, must go up first
     if start == Coord(1, 3) and end.x == 0:
@@ -64,18 +73,18 @@ def numeric_moves(start: Coord, end: Coord) -> list[str]:
     return moves(end - start)
 
 
-def directional(s: str) -> list[list[str]]:
+def directional(moves: Moves) -> MoveChain:
     """Return directional keypad moves for robot to type given sequence on another directional keypad."""
     current: Coord = DPAD["A"]
-    output: list[list[str]] = []
-    for target in s:
-        moves = directional_moves(current, DPAD[target])
-        output.append([m + "A" for m in moves])
+    output: MoveChain = []
+    for target in moves:
+        move_choice = directional_moves(current, DPAD[target])
+        output.append([m + "A" for m in move_choice])
         current = DPAD[target]
     return output
 
 
-def directional_moves(start: Coord, end: Coord) -> list[str]:
+def directional_moves(start: Coord, end: Coord) -> MoveChoice:
     """Return directional moves request to move arm for directional pad. Avoids gap."""
     # If start on bottom-left, and moving to top, must go right first
     if start == Coord(0, 1) and end.y == 0:
@@ -86,11 +95,11 @@ def directional_moves(start: Coord, end: Coord) -> list[str]:
     return moves(end - start)
 
 
-def moves(move: Coord) -> list[str]:
+def moves(move: Coord) -> MoveChoice:
     """Return directional moves request to move arm for numeric pad."""
     if move == Coord.origin():
         return [""]
-    output: list[str] = []
+    output: MoveChoice = []
     if move.x > 0:
         output.extend(">" + m for m in moves(move + Coord(-1, 0)))
     if move.x < 0:
@@ -102,50 +111,61 @@ def moves(move: Coord) -> list[str]:
     return output
 
 
-def apply_directional(xss: list[list[str]]) -> list[list[str]]:
+def apply_directional(move_chain: MoveChain) -> MoveChain:
     """Apply directional moves with all possible minimum-length move sequences."""
-    output: list[list[str]] = []
-    for xs in xss:
-        print("applying to", xs)
-        segment: list[list[str]] = []
-        for x in xs:
-            print(f"  case '{x}'")
-            segment.extend(directional(x))
-        print("segment", segment)
-        min_length = min(length(s) for s in segment)
-        print("min_length", min_length)
-        chosen = next(s for s in segment if length(s) == min_length)
-        print("chosen", chosen)
-        output.append(chosen)
-
+    output: MoveChain = []
+    for move_choice in move_chain:
+        if len(move_choice) > 1:
+            print("Considering", move_choice)
+        shortest_length: int | None = None
+        shortest_chain: MoveChain = []
+        for choice in move_choice:
+            choice_chain: MoveChain = directional(choice)
+            choice_length: int = chain_length(choice_chain)
+            if len(move_choice) > 1:
+                print("  Trying", choice, choice_chain, choice_length)
+            if shortest_length is None or choice_length < shortest_length:
+                shortest_length = choice_length
+                shortest_chain = choice_chain
+        if len(move_choice) > 1:
+            print("  Shortest", shortest_chain)
+        output.extend(shortest_chain)
     return output
 
 
-def length(xss: list[list[str]]) -> int:
-    """Minimum length of move sequences."""
+def chain_length(chain: MoveChain) -> int:
+    """Minimum length of a move chain."""
     total = 0
-    for xs in xss:
-        total += min(len(x) for x in xs)
+    for choice in chain:
+        total += min(len(s) for s in choice)
     return total
 
 
 if __name__ == "__main__":
     numeric_targets = [line.strip() for line in stdin.readlines()]
-    # lengths = [len(directional(directional(numeric(n)))) for n in numeric_targets]
-    # numbers = [int(n[:-1]) for n in numeric_targets]
-    # print(lengths)
-    # print(numbers)
-    t = numeric_targets[0]
+    # # Part (a)
+    # total = 0
+    # for target in numeric_targets:
+    #     move_chain = apply_directional(apply_directional(numeric(target)))
+    #     complexity = int(target[:-1]) * chain_length(move_chain)
+    #     print(target, int(target[:-1]), chain_length(move_chain), complexity)
+    #     total += complexity
+    # print(total)
+
+    t = numeric_targets[3]
     d1 = numeric(t)
     print(t)
-    print(length(d1))
+    print(chain_length(d1))
     print("Got:", d1)
-    print("   : <A^A>^^AvvvA")
+    # print("   : <A^A>^^AvvvA", len("<A^A>^^AvvvA"))
     d2 = apply_directional(d1)
-    print(length(d2))
+    print(chain_length(d2))
     print("Got:", d2)
-    print("   : v<<A>>^A<A>AvA<^AA>A<vAAA>^A")
+    # print("   : v<<A>>^A<A>AvA<^AA>A<vAAA>^A", len("v<<A>>^A<A>AvA<^AA>A<vAAA>^A"))
     d3 = apply_directional(d2)
-    print(length(d3))
+    print(chain_length(d3))
     print("Got:", d3)
-    print("   : <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A")
+    # print(
+    #     "   : <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A",
+    #     len("<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A"),
+    # )
