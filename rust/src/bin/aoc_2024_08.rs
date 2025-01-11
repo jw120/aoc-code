2ccc201de68bc88956adc13b3f57900212a68564
Joe Watson <jw120@users.noreply.github.com>
2025-01-11 18:24:43 +0900

    2024 08 rust tidied

--- rust/src/bin/aoc_2024_08.rs
+++ rust/src/bin/aoc_2024_08.rs
@@ -12,79 +12,77 @@ struct Map {
     antenna: HashMap<char, Vec<Coord>>,
 }
 
-impl Map {
-    fn read() -> Self {
-        let mut height: i32 = 0;
-        let mut width: i32 = 0;
-        let mut antenna: HashMap<char, Vec<Coord>> = HashMap::new();
-        for (line, row) in io::stdin().lines().zip(0..) {
-            for (ch, col) in line.unwrap().trim().chars().zip(0..) {
-                if ch != '.' {
-                    antenna
-                        .entry(ch)
-                        .and_modify(|v| v.push(coord(row, col)))
-                        .or_insert(vec![coord(row, col)]);
-                }
-                height = max(height, row + 1);
-                width = max(width, col + 1);
+fn read_map() -> Map {
+    let mut height: i32 = 0;
+    let mut width: i32 = 0;
+    let mut antenna: HashMap<char, Vec<Coord>> = HashMap::new();
+    for (line, row) in io::stdin().lines().zip(0..) {
+        for (ch, col) in line.unwrap().trim().chars().zip(0..) {
+            if ch != '.' {
+                antenna
+                    .entry(ch)
+                    .and_modify(|v| v.push(coord(row, col)))
+                    .or_insert(vec![coord(row, col)]);
             }
-        }
-        Map {
-            height,
-            width,
-            antenna,
+            height = max(height, row + 1);
+            width = max(width, col + 1);
         }
     }
-
-    fn check_bounds(&self, coord: Coord) -> Option<Coord> {
-        if coord.row >= 0 && coord.row < self.height && coord.col >= 0 && coord.col < self.width {
-            Some(coord)
-        } else {
-            None
-        }
-    }
-
-    fn anti_nodes_a(&self) -> HashSet<Coord> {
-        let mut locations: HashSet<Coord> = HashSet::new();
-        for nodes in self.antenna.values() {
-            for v in nodes.iter().combinations(2) {
-                let [n1, n2] = v[..] else {
-                    panic!("Expected 2-vector from combinations(2)")
-                };
-                let diff = *n2 - *n1;
-                self.check_bounds(*n1 - diff).map(|n| locations.insert(n));
-                self.check_bounds(*n2 + diff).map(|n| locations.insert(n));
-            }
-        }
-        locations
-    }
-
-    fn anti_nodes_b(&self) -> HashSet<Coord> {
-        let mut locations: HashSet<Coord> = HashSet::new();
-        for nodes in self.antenna.values() {
-            for v in nodes.iter().combinations(2) {
-                let [n1, n2] = v[..] else {
-                    panic!("Expected 2-vector from combinations(2)")
-                };
-                let diff = *n2 - *n1;
-                let mut n = *n1;
-                while let Some(c) = self.check_bounds(n) {
-                    locations.insert(c);
-                    n += diff;
-                }
-                n = *n1 - diff;
-                while let Some(c) = self.check_bounds(n) {
-                    locations.insert(c);
-                    n -= diff;
-                }
-            }
-        }
-        locations
+    Map {
+        height,
+        width,
+        antenna,
     }
 }
 
+fn check_bounds(m: &Map, coord: Coord) -> Option<Coord> {
+    if coord.row >= 0 && coord.row < m.height && coord.col >= 0 && coord.col < m.width {
+        Some(coord)
+    } else {
+        None
+    }
+}
+
+fn anti_nodes_a(m: &Map) -> HashSet<Coord> {
+    let mut locations: HashSet<Coord> = HashSet::new();
+    for nodes in m.antenna.values() {
+        for v in nodes.iter().combinations(2) {
+            let [n1, n2] = v[..] else {
+                panic!("Expected 2-vector from combinations(2)")
+            };
+            let diff = *n2 - *n1;
+            check_bounds(m, *n1 - diff).map(|n| locations.insert(n));
+            check_bounds(m, *n2 + diff).map(|n| locations.insert(n));
+        }
+    }
+    locations
+}
+
+fn anti_nodes_b(m: &Map) -> HashSet<Coord> {
+    let mut locations: HashSet<Coord> = HashSet::new();
+    for nodes in m.antenna.values() {
+        for v in nodes.iter().combinations(2) {
+            let [n1, n2] = v[..] else {
+                panic!("Expected 2-vector from combinations(2)")
+            };
+            let diff = *n2 - *n1;
+            let mut n = *n1;
+            while let Some(c) = check_bounds(m, n) {
+                locations.insert(c);
+                n += diff;
+            }
+            n = *n1 - diff;
+            while let Some(c) = check_bounds(m, n) {
+                locations.insert(c);
+                n -= diff;
+            }
+        }
+    }
+    locations
+}
+
 fn main() {
-    let map = Map::read();
-    println!("{}", map.anti_nodes_a().len());
-    println!("{}", map.anti_nodes_b().len());
+    let m = read_map();
+    println!("{}", anti_nodes_a(&m).len());
+    println!("{}", anti_nodes_b(&m).len());
 }
