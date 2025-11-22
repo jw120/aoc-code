4bc9bf6174f793dda8226d298acff2a1b31f1b4c
Joe Watson <jw120@users.noreply.github.com>
2025-11-22 17:54:21 +0800

    started 2024 11 rs

---
+++ rust/src/bin/aoc_2024_21.rs
@@ -0,0 +1,19 @@
+struct NumPadPosition(u8);
+
+// Rows represent connections from each numeric pad position
+// Within each row, order is Up, Right, Down, Left
+const NUM_CONNECTIONS: [[Option<usize>; 4]; 11] = [
+    [Some(2), Some(10), None, None],      // 0
+    [Some(4), Some(2), None, None],       // 1
+    [Some(5), Some(3), Some(0), Some(1)], // 2
+    [Some(6), None, Some(10), Some(2)],   // 3
+    [Some(7), Some(5), Some(1), None],    // 4
+    [Some(8), Some(6), Some(2), Some(4)], // 5
+    [Some(9), None, Some(3), Some(5)],    // 6
+    [None, Some(8), Some(4), None],       // 7
+    [None, Some(9), Some(5), Some(7)],    // 8
+    [None, Some(6), Some(8), None],       // 9
+    [Some(3), None, None, Some(0)],       // A
+];
+
+enum Direction { U, R, D, L};
