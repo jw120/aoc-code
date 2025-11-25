2a2391f2d60aefd1e928df81f92c53b97e9e62fd
Joe Watson <jw120@users.noreply.github.com>
2025-11-26 03:42:09 +0800

    racket 2024 01

---
+++ others/racket/2024/day01.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+(define (read-input)
+  (let ([lines (for/list ([line (in-lines)]) (string-split line))])
+    (values
+     (map (λ (line) (string->number (first line))) lines)
+     (map (λ (line) (string->number (second line))) lines))))
+
+; Part a: Sum of absolute differences of sorted lists
+(define (part-a x y)
+  (for/sum ([i (sort x <)] [j (sort y <)]) (abs (- i j))))
+
+(module+ test
+  (require rackunit)
+  (check-equal? (part-a '(3 2 1) '(2 3 4)) 3))
+
+; Part b: Product of first list and number of occurrences in second list
+(define (part-b x y)
+  (for/sum ([i x])
+    (* i (count (λ (x) (equal? i x)) y))))
+
+(module+ test
+  (check-equal? (part-b '(3 2 1 4) '(2 3 4 4)) (+ 3 2 8)))
+
+(module+ main
+  (define-values (x y) (read-input))
+  (displayln (part-a x y))
+  (displayln (part-b x y)))
+
+
+
