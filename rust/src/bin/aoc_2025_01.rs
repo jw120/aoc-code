72f10d07486d7af37d185c7b1b3769b68406d9dd
Joe Watson <jw120@users.noreply.github.com>
2025-12-01 21:49:59 +0800

    2025 01 rust done badly

--- rust/src/bin/aoc_2025_01.rs
+++ rust/src/bin/aoc_2025_01.rs
@@ -5,48 +5,67 @@ use std::io;
 const START_POSITION: i32 = 50;
 const DIAL_SIZE: i32 = 100;
 
-#[derive(Debug)]
-enum Move {
-    Left(i32),
-    Right(i32),
-}
-
-impl Move {
-    fn new(line: &str) -> Self {
-        let num: i32 = line[1..].parse().unwrap();
-        match &line[..1] {
-            "L" => Self::Left(num),
-            "R" => Self::Right(num),
-            _ => panic!("Bad move line {line}"),
-        }
-    }
-
-    fn apply(&self, position: i32) -> i32 {
-        match self {
-            Self::Left(n) => (position - n) % DIAL_SIZE,
-            Self::Right(n) => (position + n) % DIAL_SIZE,
-        }
+fn parse_move(line: &str) -> i32 {
+    let num: i32 = line[1..].parse().unwrap();
+    match &line[..1] {
+        "L" => -num,
+        "R" => num,
+        _ => panic!("Bad move line {line}"),
     }
 }
 
-fn part_a(moves: &[Move]) -> usize {
+fn run(moves: &[i32]) -> (usize, usize) {
     let mut position: i32 = START_POSITION;
-    let mut zero_count: usize = 0;
-    for m in moves {
-        position = m.apply(position);
-        if position == 0 {
-            zero_count += 1;
+    let mut zero_finishes: usize = 0;
+    let mut zero_passes: usize = 0;
+    for mm in moves {
+        let mut m: i32 = *mm;
+        let old_position = position;
+        while m != 0 {
+            if m >= DIAL_SIZE {
+                m -= DIAL_SIZE;
+                zero_passes += 1;
+            } else if m >= 0 {
+                position += m;
+                if position == DIAL_SIZE {
+                    zero_finishes += 1;
+                    position = 0;
+                }
+                if position > DIAL_SIZE {
+                    position -= DIAL_SIZE;
+                    zero_passes += 1;
+                }
+                m = 0;
+            } else if m <= -DIAL_SIZE {
+                m += DIAL_SIZE;
+                zero_passes += 1;
+            } else {
+                position += m;
+                if position == 0 {
+                    zero_finishes += 1;
+                    position = 0;
+                }
+                if position < 0 {
+                    position += DIAL_SIZE;
+                    zero_passes += usize::from(old_position != 0);
+                }
+                m = 0;
+            }
         }
-        println!("{m:?} {position} {zero_count}");
+        assert!(position >= 0);
+        assert!(position < DIAL_SIZE);
+        println!("{mm} {position} {zero_finishes} {zero_passes}");
     }
-    zero_count
+    (zero_finishes, zero_passes + zero_finishes)
 }
 
 fn main() {
-    let moves: Vec<Move> = io::stdin()
+    let moves: Vec<i32> = io::stdin()
         .lines()
-        .map(|s| Move::new(&s.unwrap()))
+        .map(|s| parse_move(&s.unwrap()))
         .collect();
 
-    println!("{}", part_a(&moves));
+    let (part_a, part_b) = run(&moves);
+    println!("{part_a}");
+    println!("{part_b}");
 }
