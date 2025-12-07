88454deb4be8f3be3b90ecb1b756092d5c4c7d92
Joe Watson <jw120@users.noreply.github.com>
2025-12-07 15:09:39 +0800

    2025 07 rust done with swapping

--- rust/src/bin/aoc_2025_07.rs
+++ rust/src/bin/aoc_2025_07.rs
@@ -17,46 +17,54 @@ fn read_input() -> (Vec<Vec<bool>>, usize) {
 
 fn part_a(diagram: &[Vec<bool>], start: usize) -> u32 {
     let n: usize = diagram[0].len();
-    let mut beams: Vec<bool> = vec![false; n];
-    beams[start] = true;
+    let mut beams_even: Vec<bool> = vec![false; n];
+    let mut beams_odd: Vec<bool> = vec![false; n];
+    beams_even[start] = true;
 
     let mut split_count: u32 = 0;
-    let mut new_beams: Vec<bool> = vec![false; n];
-    for row in &diagram[1..] {
-        for (i, _) in beams.iter().enumerate().filter(|(_, b)| **b) {
+    for (r, row) in diagram.iter().enumerate() {
+        let (current_beams, next_beams) = if r.is_multiple_of(2) {
+            (&mut beams_even, &mut beams_odd)
+        } else {
+            (&mut beams_odd, &mut beams_even)
+        };
+        next_beams.fill(false);
+        for (i, _) in current_beams.iter().enumerate().filter(|(_, b)| **b) {
             if row[i] {
-                new_beams[i - 1] = true;
-                new_beams[i + 1] = true;
+                next_beams[i - 1] = true;
+                next_beams[i + 1] = true;
                 split_count += 1;
             } else {
-                new_beams[i] = true;
+                next_beams[i] = true;
             }
         }
-        beams.clone_from(&new_beams);
-        new_beams.fill(false);
     }
     split_count
 }
 
 fn part_b(diagram: &[Vec<bool>], start: usize) -> u64 {
     let n: usize = diagram[0].len();
-    let mut beams: Vec<u64> = vec![0; n];
-    beams[start] = 1;
+    let mut beams_even: Vec<u64> = vec![0; n];
+    let mut beams_odd: Vec<u64> = vec![0; n];
+    beams_even[start] = 1;
 
-    let mut new_beams: Vec<u64> = vec![0; n];
-    for row in &diagram[1..] {
-        for (i, n) in beams.iter().enumerate().filter(|(_, n)| **n > 0) {
+    for (r, row) in diagram.iter().enumerate() {
+        let (current_beams, next_beams) = if r.is_multiple_of(2) {
+            (&mut beams_even, &mut beams_odd)
+        } else {
+            (&mut beams_odd, &mut beams_even)
+        };
+        next_beams.fill(0);
+        for (i, n) in current_beams.iter().enumerate().filter(|(_, n)| **n > 0) {
             if row[i] {
-                new_beams[i - 1] += n;
-                new_beams[i + 1] += n;
+                next_beams[i - 1] += n;
+                next_beams[i + 1] += n;
             } else {
-                new_beams[i] += n;
+                next_beams[i] += n;
             }
         }
-        beams.clone_from(&new_beams);
-        new_beams.fill(0);
     }
-    beams.iter().sum()
+    beams_even.iter().sum()
 }
 
 fn main() {
